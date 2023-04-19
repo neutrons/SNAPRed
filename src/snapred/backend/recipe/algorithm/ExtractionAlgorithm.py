1dbcc4289e2deb8eb18c5a1bee344cbcc3504fc2
Reece Boston (4rx) <bostonsr@ornl.com>
2023-04-19 09:12:49 -0400

    removed needless sleeping

--- src/snapred/backend/recipe/algorithm/ExtractionAlgorithm.py
+++ src/snapred/backend/recipe/algorithm/ExtractionAlgorithm.py
@@ -1,6 +1,4 @@
-import time
-
-from mantid.api import AlgorithmFactory, Progress, PythonAlgorithm
+from mantid.api import AlgorithmFactory, PythonAlgorithm
 
 # from mantid.kernel import *
 
@@ -15,14 +13,6 @@ class ExtractionAlgorithm(PythonAlgorithm):
     def PyExec(self):
         # run the algo
         self.log().notice("exec extract diffractometer constants, empty Algo")
-        endrange = 5
-
-        prog_reporter = Progress(self, start=0.0, end=1.0, nreports=endrange)
-        for i in range(0, endrange):
-            time.sleep(1)
-            prog_reporter.reportIncrement(i, "processing...")
-
-        prog_reporter.report(endrange, "Done")
 
 
 # Register algorithm with Mantid
