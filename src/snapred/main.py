cff8a50a29c297c0948df4b0480d5495b7231326
Michael Walsh <walshmm@ornl.gov>
2023-04-20 11:55:52 -0400

    fixed wheel, fixing conf file lookup

--- src/snapred/main.py
+++ src/snapred/main.py
@@ -1,41 +1,35 @@
-from PyQt5 import QtGui, QtCore, QtWidgets
-from PyQt5.QtGui import QPalette
-from PyQt5.QtWidgets import *
-from PyQt5.QtCore import *
 import sys
 
-from snapred.ui.widget.MainWidget import DummyWidget
-
-from mantidqt.widgets.instrumentview.api import get_instrumentview
 from mantidqt.widgets.algorithmprogress import AlgorithmProgressWidget
 from mantidqt.widgets.workspacewidget.workspacetreewidget import WorkspaceTreeWidget
-from mantid.simpleapi import CreateSampleWorkspace
-
+from PyQt5 import QtCore, QtWidgets
+from PyQt5.QtCore import *
+from PyQt5.QtGui import QPalette
+from PyQt5.QtWidgets import *
 
 from snapred.backend.log.logger import snapredLogger
 from snapred.meta.Config import Resource
+from snapred.ui.widget.MainWidget import DummyWidget
 
 logger = snapredLogger.getLogger(__name__)
-
-import snapred as sr
-import inspect
 
 
 class MyBar(QWidget):
     clickPos = None
+
     def __init__(self, parent):
         super(MyBar, self).__init__(parent)
         self.setAutoFillBackground(True)
-        
+
         self.setBackgroundRole(QPalette.Shadow)
-        
+
         layout = QHBoxLayout(self)
         layout.setContentsMargins(1, 1, 1, 1)
         layout.addStretch()
 
         self.title = QLabel("My Own Bar", self, alignment=Qt.AlignCenter)
         self.title.setObjectName("headerTitle")
-        
+
         self.setObjectName("header")
         # if setPalette() was used above, this is not required
         self.title.setForegroundRole(QPalette.Light)
@@ -46,34 +40,37 @@ class MyBar(QWidget):
         self.setMaximumHeight(ref_size + 2)
 
         btn_size = QSize(ref_size, ref_size)
-        for target in ('min', 'normal', 'max', 'close'):
+        for target in ("min", "normal", "max", "close"):
             btn = QToolButton(self, focusPolicy=Qt.NoFocus)
             layout.addWidget(btn)
             btn.setFixedSize(btn_size)
 
-            iconType = getattr(style, 
-                'SP_TitleBar{}Button'.format(target.capitalize()))
+            iconType = getattr(style, "SP_TitleBar{}Button".format(target.capitalize()))
             btn.setIcon(style.standardIcon(iconType))
 
-            if target == 'close':
-                colorNormal = 'red'
-                colorHover = 'orangered'
+            if target == "close":
+                colorNormal = "red"
+                colorHover = "orangered"
             else:
-                colorNormal = 'palette(mid)'
-                colorHover = 'palette(light)'
-            btn.setStyleSheet('''
+                colorNormal = "palette(mid)"
+                colorHover = "palette(light)"
+            btn.setStyleSheet(
+                """
                 QToolButton {{
                     background-color: {};
                 }}
                 QToolButton:hover {{
                     background-color: {}
                 }}
-            '''.format(colorNormal, colorHover))
+            """.format(
+                    colorNormal, colorHover
+                )
+            )
 
-            signal = getattr(self, target + 'Clicked')
+            signal = getattr(self, target + "Clicked")
             btn.clicked.connect(signal)
 
-            setattr(self, target + 'Button', btn)
+            setattr(self, target + "Button", btn)
 
         self.normalButton.hide()
 
@@ -85,8 +82,7 @@ class MyBar(QWidget):
             title = self.window().windowTitle()
         width = self.title.width()
         width -= self.style().pixelMetric(QStyle.PM_LayoutHorizontalSpacing) * 3
-        self.title.setText(self.fontMetrics().elidedText(
-            title, Qt.ElideRight, width))
+        self.title.setText(self.fontMetrics().elidedText(title, Qt.ElideRight, width))
 
     def windowStateChanged(self, state):
         self.normalButton.setVisible(state == Qt.WindowMaximized)
@@ -119,8 +115,8 @@ class MyBar(QWidget):
         self.title.resize(self.width(), self.height())
         self.updateTitle()
 
-class SNAPRedGUI(QtWidgets.QMainWindow):
 
+class SNAPRedGUI(QtWidgets.QMainWindow):
     def __init__(self, parent=None):
         super(SNAPRedGUI, self).__init__(parent)
         self.setAttribute(Qt.WA_TranslucentBackground, True)
@@ -128,7 +124,7 @@ class SNAPRedGUI(QtWidgets.QMainWindow):
         dummyWidget = DummyWidget("load dummy", self)
         splitter = QtWidgets.QSplitter(QtCore.Qt.Vertical)
         splitter.addWidget(dummyWidget.widget)
-        
+
         # myiv = get_instrumentview(ws)
         # myiv.show_view()
 
@@ -141,12 +137,12 @@ class SNAPRedGUI(QtWidgets.QMainWindow):
         workspaceTreeWidgetWrapper.setLayout(workspaceTreeWidgetWrapperLayout)
         splitter.addWidget(workspaceTreeWidgetWrapper)
         splitter.addWidget(AlgorithmProgressWidget())
-        
+
         centralWidget = QWidget()
         centralWidget.setObjectName("centralwidget")
         centralLayout = QVBoxLayout()
         centralWidget.setLayout(centralLayout)
-        
+
         self.setCentralWidget(centralWidget)
         self.setWindowTitle("SNAPRed")
         self.setWindowFlags(self.windowFlags() | Qt.FramelessWindowHint)
@@ -159,20 +155,20 @@ class SNAPRedGUI(QtWidgets.QMainWindow):
 
         self.resize(320, self.titleBar.height() + 480)
         self.setMinimumSize(320, self.titleBar.height() + 480)
-        
+
         self.statusBar = QStatusBar()
         self.setStatusBar(self.statusBar)
-        
-        
+
     def closeEvent(self, event):
         event.accept()
-        
+
     def changeEvent(self, event):
         if event.type() == event.WindowStateChange:
             self.titleBar.windowStateChanged(self.windowState())
 
     def resizeEvent(self, event):
         self.titleBar.resizeEvent(event)
+
 
 def qapp():
     if QtWidgets.QApplication.instance():
@@ -184,20 +180,20 @@ def qapp():
 
 if __name__ == "__main__":
     app = qapp()
-    with Resource.open('style.qss', 'r') as styleSheet:
-            app.setStyleSheet(styleSheet.read())
+    with Resource.open("style.qss", "r") as styleSheet:
+        app.setStyleSheet(styleSheet.read())
     try:
         ex = SNAPRedGUI()
-        
-        #ex.resize(700, 700)
-        asciiPath = 'ascii.txt'
-        with Resource.open(asciiPath, 'r') as asciiArt:
+
+        # ex.resize(700, 700)
+        asciiPath = "ascii.txt"
+        with Resource.open(asciiPath, "r") as asciiArt:
             print(asciiArt.read())
         logger.info("Welcome User! Happy Reducing!")
         ex.show()
         ret = app.exec_()
         sys.exit(ret)
-        
+
     except Exception as uncaughtError:
         ex = QtWidgets.QWidget()
         QtWidgets.QMessageBox.critical(ex, "Uncaught Error!", str(uncaughtError))
