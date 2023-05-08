6417a8c525c45a4fc80a531c39a31b68df7ae5de
Michael Walsh <walshmm@ornl.gov>
2023-05-08 13:21:14 -0400

    config mantid to not update during testing, possible githubactions segfault fix

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,8 +5,15 @@ import os
 # os.environ['PYTHONPATH'] = './src'
 os.environ["env"] = "test"
 
-from snapred.meta.Config import Config  # noqa: E402
-from snapred.meta.Config import Resource  # noqa: E402
+from mantid.kernel import ConfigService  # noqa: E402
+from snapred.meta.Config import (  # noqa: E402
+    Config,  # noqa: E402
+    Resource,  # noqa: E402
+)
 
 # manually alter the config to point to the test resources
 Config._config["instrument"]["home"] = Resource.getPath(Config["instrument.home"])
+mantidConfig = config = ConfigService.Instance()
+mantidConfig["CheckMantidVersion.OnStartup"] = "0"
+mantidConfig["UpdateInstrumentDefinitions.OnStartup"] = "0"
+mantidConfig["usagereports.enabled"] = "0"
