5bdc4b771620c5d7287814363f8627ed14dd4c8d
rboston628 <bostonsr@ornl.gov>
2023-05-11 14:11:08 -0400

    start off

--- tests/unit/backend/recipe/test_ExtractionRecipe.py
+++ tests/unit/backend/recipe/test_ExtractionRecipe.py
@@ -22,6 +22,55 @@ with mock.patch.dict("sys.modules", {"mantid.api": mock.Mock()}):
         ingredients.json = mock.Mock(return_value="good json")
         data = recipe.executeRecipe(ingredients)
 
+def setup():
+    """Setup before all tests"""
+    pass
+
+
+def teardown():
+    """Teardown after all tests"""
+    pass
+
+
+@pytest.fixture(autouse=True)
+def setup_teardown():  # noqa: PT004
+    """Setup before each test, teardown after each test"""
+    setup()
+    yield
+    teardown()
+
+
+@mock.patch("snapred.backend.recipe.ExtractionRecipe.AlgorithmManager", AlgorithmManager)
+def test_execute_successful():
+    mockAlgo.execute.return_value = "passed"
+    mockAlgo.ingredients = ""
+    mockAlgo.setProperty.side_effect = lambda x, y: setattr(mockAlgo, "ingredients", y)  # noqa: ARG005
+
+    recipe = ExtractionRecipe()
+    ingredients = mock.Mock()
+    ingredients.json = mock.Mock(return_value="good json")
+    data = recipe.executeRecipe(ingredients)
+
+    assert AlgorithmManager.create.called
+    assert mockAlgo.execute.called
+    assert ingredients.json.called
+    assert mockAlgo.setProperty.called
+    assert mockAlgo.ingredients == "good json"
+    assert isinstance(data, dict)
+    assert data["result"] is not None
+    assert data["result"] == "passed"
+
+
+@mock.patch("snapred.backend.recipe.ExtractionRecipe.AlgorithmManager", AlgorithmManager)
+def test_execute_unsuccessful():
+    mockAlgo.execute.side_effect = RuntimeError()
+
+    recipe = ExtractionRecipe()
+    ingredients = mock.Mock()
+
+    try:
+        recipe.executeRecipe(ingredients)
+    except:  # noqa: E722
         assert AlgorithmManager.create.called
         assert mockAlgo.execute.called
         assert ingredients.json.called
