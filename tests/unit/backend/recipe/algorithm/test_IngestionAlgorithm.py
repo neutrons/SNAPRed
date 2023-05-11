055df349d7032b3c24f22bc154d3f49cc655d449
rboston628 <bostonsr@ornl.gov>
2023-05-11 16:02:15 -0400

    yolo

--- tests/unit/backend/recipe/algorithm/test_IngestionAlgorithm.py
+++ tests/unit/backend/recipe/algorithm/test_IngestionAlgorithm.py
@@ -26,24 +26,18 @@ def setup_teardown():  # noqa: PT004
 def test_weak_f_squared_lowest():
     """Test weak fSquared method finds lowest 1%"""
     ingestAlgo = IngestCrystallographicInfo()
-    assert not isinstance(ingestAlgo, mock.Mock)
     mock_xtal = mock.Mock()
     mock_xtal.fSquared = list(range(36, 136))
     mock_xtal.multiplicities = [1,2,3,4,5]*20
-
-    assert len(mock_xtal.fSquared) == len(mock_xtal.multiplicities)
 
     assert ingestAlgo.findWeakFSquared(mock_xtal) == 36
 
 def test_weak_f_squared_median():
     """Test weak fSquared method finds median of lowest 1%"""
     ingestAlgo = IngestCrystallographicInfo()
-    assert not isinstance(ingestAlgo, mock.Mock)
     mock_xtal = mock.Mock()
     mock_xtal.fSquared = list(range(36, 336))
     mock_xtal.multiplicities = [1,2,3,4,5]*60
-
-    assert len(mock_xtal.fSquared) == len(mock_xtal.multiplicities)
 
     lowFsq = ingestAlgo.findWeakFSquared(mock_xtal)
     assert lowFsq != 36
@@ -52,18 +46,15 @@ def test_weak_f_squared_median():
 def test_weak_f_squared_small():
     """Test weak fSquared method finds an answer with small number of options"""
     ingestAlgo = IngestCrystallographicInfo()
-    assert not isinstance(ingestAlgo, mock.Mock)
     mock_xtal = mock.Mock()
     mock_xtal.fSquared = [2, 3, 4, 5]
     mock_xtal.multiplicities = [1, 1, 1, 1]
 
     assert ingestAlgo.findWeakFSquared(mock_xtal) == 2
 
-
 def test_weak_f_squared_one():
     """Test weak fSquared method finds an answer with only one option"""
     ingestAlgo = IngestCrystallographicInfo()
-    assert not isinstance(ingestAlgo, mock.Mock)
     mock_xtal = mock.Mock()
     mock_xtal.fSquared = [2]
     mock_xtal.multiplicities = [1]
@@ -136,5 +127,3 @@ def test_example_xtal_fsq():
     lowFsq= lowF*lowM
 
     assert ingestAlgo.findWeakFSquared(xtalInfo) == lowFsq
-
-
