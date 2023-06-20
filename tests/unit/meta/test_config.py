4025d9fe5378807f6caf0f3007b61a8c0f1d7a31
Michael Walsh <walshmm@ornl.gov>
2023-06-20 15:19:52 -0400

    add tests for resoucre

--- tests/unit/meta/test_config.py
+++ tests/unit/meta/test_config.py
@@ -1,5 +1,5 @@
 # import pytest
-from snapred.meta.Config import Config
+from snapred.meta.Config import Config, Resource
 
 
 def test_environment():
@@ -9,3 +9,20 @@ def test_environment():
 def test_instrument_home():
     correctPath = "/SNAPRed/tests/resources/SNAP/"
     assert Config["instrument.home"][-len(correctPath) :] == correctPath
+
+
+def test_resource_exists():
+    assert Resource.exists("application.yml")
+
+
+def test_resource_not_exists():
+    assert not Resource.exists("not_a_real_file.yml")
+
+
+def test_resource_read():
+    assert Resource.read("application.yml") is not None
+
+
+def test_resouce_open():
+    with Resource.open("application.yml", "r") as file:
+        assert file is not None
