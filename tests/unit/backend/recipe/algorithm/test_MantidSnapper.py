a4a8aa92aca6b9794b9a26f7289691631f7d45a4
Reece Boston <52183986+rboston628@users.noreply.github.com>
2023-06-26 09:57:15 -0400

    [patch] add tests for ReductionAlgorithm (#44)
    * tests for reduction, mantidsnapper
    
    * moved test of mantid snapper, 'fixed' errors
    
    * created boilerplate tests for reduction recipe

---
+++ tests/unit/backend/recipe/algorithm/test_MantidSnapper.py
@@ -0,0 +1,28 @@
+import unittest.mock as mock
+
+with mock.patch("mantid.api.AlgorithmManager") as FakeAlgorithmManager:
+    from mantid.kernel import Direction
+    from snapred.backend.recipe.algorithm.MantidSnapper import MantidSnapper
+
+    fakeOutput = mock.Mock()
+    fakeOutput.name = "fakeOutput"
+    fakeOutput.direction = Direction.Output
+
+    fakeFunction = mock.Mock()
+    fakeFunction.getProperties.return_value = {}
+    fakeFunction.getProperty.return_value = fakeOutput
+
+    FakeAlgorithmManager.create.return_value = fakeFunction
+
+    def test_snapper_fake_algo():
+        return_of_algo = "return of algo"
+        mantidSnapper = MantidSnapper(parentAlgorithm=None, name="")
+        test = mantidSnapper.fakeFunction("test", fakeOutput=return_of_algo)
+        assert test == return_of_algo
+
+    def test_snapper_fake_queue():
+        return_of_algo = "return of algo"
+        mantidSnapper = MantidSnapper(parentAlgorithm=None, name="")
+        mantidSnapper.fakeFunction("test", fakeOutput=return_of_algo)
+        mantidSnapper.executeQueue()
+        assert fakeFunction.execute.called
---
+++ tests/unit/backend/recipe/test_ReductionRecipe.py
@@ -0,0 +1,55 @@
+import unittest.mock as mock
+
+import pytest
+
+with mock.patch("mantid.api.AlgorithmManager") as MockAlgorithmManager:
+    from snapred.backend.recipe.ReductionRecipe import ReductionRecipe as ThisRecipe
+
+    mockAlgo = mock.Mock()
+    MockAlgorithmManager.create.return_value = mockAlgo
+
+    def test_execute_successful():
+        mockAlgo.execute.return_value = "passed"
+        mockAlgo.ingredients = "bad json"
+        mockAlgo.setProperty.side_effect = lambda x, y: setattr(mockAlgo, "ingredients", y)  # noqa: ARG005
+
+        recipe = ThisRecipe()
+        ingredients = mock.Mock()
+        ingredients.json = mock.Mock(return_value="good json")
+        data = recipe.executeRecipe(ingredients)
+
+        assert mockAlgo.execute.called
+        assert ingredients.json.called
+        assert mockAlgo.ingredients == "good json"
+        assert isinstance(data, dict)
+        assert data["result"] is not None
+        assert data["result"] == "passed"
+
+    def test_execute_unsuccessful():
+        mockAlgo.execute.side_effect = RuntimeError("passed")
+
+        recipe = ThisRecipe()
+        ingredients = mock.Mock()
+
+        try:
+            recipe.executeRecipe(ingredients)
+        except Exception as e:  # noqa: E722 BLE001
+            assert str(e) == "passed"  # noqa: PT017
+            assert mockAlgo.execute.called
+        else:
+            # fail if execute did not raise an exception
+            pytest.fail("Test should have raised RuntimeError, but no error raised")
+
+
+# this at teardown removes the loggers, eliminating logger error printouts
+# see https://github.com/pytest-dev/pytest/issues/5502#issuecomment-647157873
+@pytest.fixture(autouse=True)
+def clear_loggers():  # noqa: PT004
+    """Remove handlers from all loggers"""
+    import logging
+
+    loggers = [logging.getLogger()] + list(logging.Logger.manager.loggerDict.values())
+    for logger in loggers:
+        handlers = getattr(logger, "handlers", [])
+        for handler in handlers:
+            logger.removeHandler(handler)
