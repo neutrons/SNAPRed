f48531a24d70238a3e68aa471cfcbc0562c454d6
Michael Walsh <walshmm@ornl.gov>
2023-06-29 16:36:00 -0400

    implement calibration metric extraction algo

--- src/snapred/backend/recipe/algorithm/CalibrationMetricExtractionAlgorithm.py
+++ src/snapred/backend/recipe/algorithm/CalibrationMetricExtractionAlgorithm.py
@@ -1,5 +1,5 @@
-
-
+import json
+from math import sqrt
 from typing import List
 
 from mantid.api import AlgorithmFactory, PythonAlgorithm
@@ -25,54 +25,58 @@ class CalibrationMetricExtractionAlgorithm(PythonAlgorithm):
     def PyExec(self):
         inputGroupWorkspace = self.getProperty("InputGroupWorkspace").value
         crystallographicInfo = parse_raw_as(CrystallographicInfo, self.getProperty("CrystallographicInfo").value)
-        pixelGroupingInstrumentParameters = parse_raw_as(List[PixelGroupingInstrumentParameters], self.getProperty("PixelGroupingInstrumentParameters").value)
-        fittedPeakPos = f"{wsName}_fitted_peakpositions_{index}"
-        fittedParams = f"{wsName}_fitted_params_{index}"
-        fittedWS = f"{wsName}_fitted_{index}"
-        fittedParamsErr = f"{wsName}_fitted_params_err_{index}"
-        self.mantidSnapper.mtd[inputGroupWorkspace]
-        strippedWorkspaces = []
-        for workspaceIndex in range(len(pixelGroupingInstrumentParameters)):
-            group = inputGroupWorkspace.getItem(workspaceIndex)
-            peakMetrics = []
-            # for every subgroup
-            #     sigAvg = 0
-            #     sigStd = 0
-            #     posAvg = 0
-            #     posStd = 0
-            #     twoThetaAvg = 0
-            #     peakPos = group->subgroup->peakPos
-            #     peaks = group->subgroup->params
-            #     for i, peak in enumerate(peaks)
-            #         # calc sig/pos and average over all peaks, with a standard deviation estimate
-            #         # calc (d_ref-pos)/sig and average over all peaks where d_ref is a known peak position of the calbrant sample, with a standard deviation estimate
-            #         # extract position, sig, height
-            #         d_ref = crystalInfo.peaks[i].dSpacing
-            #         position = peakPos.readY(i)[0]
-            #         sig = peak["Sigma"]
-            #         height = peak["Height"]
-            #         sigAvg += sig/position
-            #         posAvg += (d_ref - position)/sig
-            #     sigAvg = sigAvg/numPeaks
-            #     posAvg = posAvg/numPeaks
-            #     twoThetaAvg = instrumentState.pixelGroupingInstrumentParameters[index].twoThetaAverage
-            
+        parse_raw_as(
+            List[PixelGroupingInstrumentParameters], self.getProperty("PixelGroupingInstrumentParameters").value
+        )
+        fittedPeakPos = "_fitted_peakpositions"
+        fittedParams = "_fitted_params"
+        fittedParamsErr = "_fitted_params_err"
+        inputGroupWorkspace = self.mantidSnapper.mtd[inputGroupWorkspace]
+        # collect all params and peak positions
+        paramNames = []
+        peakPositionNames = []
+        for workspaceIndex in range(inputGroupWorkspace.getNumberOfEntries()):
+            ws = inputGroupWorkspace.getItem(workspaceIndex)
+            wsName = ws.getName()
+            if fittedPeakPos in wsName:
+                peakPositionNames.append(wsName)
+            elif fittedParams in wsName and fittedParamsErr not in wsName:
+                paramNames.append(wsName)
+
+        peakMetrics = []
+        for paramName, peakPositionName, workspaceIndex in zip(paramNames, peakPositionNames, range(len(paramNames))):
+            peakPos = self.mantidSnapper.mtd[peakPositionName]
+            params = self.mantidSnapper.mtd[paramName]
+            sigAvg = 0
+            sigStd = 0
+            posAvg = 0
+            posStd = 0
+            twoThetaAvg = 0
+            for rowIndex in range(params.rowCount()):
+                row = params.row(rowIndex)
+                sig = row["Sigma"]
+                twoTheta = row["PeakCentre"]
+                pos = peakPos.readY(rowIndex)[0]
+                d_ref = crystallographicInfo.peaks[rowIndex].dSpacing
+                sigAvg += sig / pos
+                posAvg += (d_ref - pos) / sig
+                twoThetaAvg += twoTheta
+            numPeaks = params.rowCount()
+            sigAvg = sigAvg / numPeaks
+            posAvg = posAvg / numPeaks
+            twoThetaAvg = twoThetaAvg / numPeaks
+
             #     calc standard deviation of sig and pos
-            #     for every peak
-            #         sigStd += (sig - sigAvg)^2
-            #         posStd += (pos - posAvg)^2
-            #     sigStd = sqrt(sigStd/numPeaks)
-            #     posStd = sqrt(posStd/numPeaks)
-            #     peakMetrics.append((sigAvg, sigStd, posAvg, posStd, twoThetaAvg))
+            for rowIndex in range(params.rowCount()):
+                sig = row["Sigma"]
+                pos = peakPos.readY(rowIndex)[0]
+                sigStd += (sig - sigAvg) ^ 2
+                posStd += (pos - posAvg) ^ 2
+            sigStd = sqrt(sigStd / numPeaks)
+            posStd = sqrt(posStd / numPeaks)
+            peakMetrics.append((sigAvg, sigStd, posAvg, posStd, twoThetaAvg))
 
-            # some nitty gritty details of the above
-            # peakposws = mtd['peakpositions']
-            # param_ws = mtd['fitted_params']
-            # row = param_ws.row(0)
-
-            # # output
-            # print ('Fitted peak position: {0:.5f}'.format(peakposws.readY(0)[0]))
-            # print ("Peak 0  Centre: {0:.5f}, width: {1:.5f}, height: {2:.5f}".format(row["PeakCentre"], row["Sigma"], row["Height"]))
+        self.setProperty("OutputMetrics", json.dumps(peakMetrics))
 
 
 # Register algorithm with Mantid
