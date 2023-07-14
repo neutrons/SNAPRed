e7c18324035ffabdd84f402a16c6d3ff85afff38
Robert Bolotovsky <118292199+bolotovskyr@users.noreply.github.com>
2023-07-14 06:46:09 -0700

    Extract DetectorPeakPredictor algorithm. Add peak tail coefficient. (#56)
    * Consolidate detector peak prediction. Use peak tail coefficient.
    
    * Fix broken data service tests.
    
    * Add unit tests for peak prediction.
    
    * Store detector peak extent as LimitedValue. Remove unused code.

--- src/snapred/backend/dao/Limit.py
+++ src/snapred/backend/dao/Limit.py
@@ -15,3 +15,15 @@ class Limit(GenericModel, Generic[T]):
         if values.get("minimum") > values.get("maximum"):
             raise ValueError("min cannot be greater than max")
         return values
+
+
+class LimitedValue(GenericModel, Generic[T]):
+    value: T
+    minimum: T
+    maximum: T
+
+    @root_validator(allow_reuse=True)
+    def validate_scalar_fields(cls, values):
+        if not values.get("minimum") <= values.get("value") <= values.get("maximum"):
+            raise ValueError("value has to be between min and max")
+        return values
