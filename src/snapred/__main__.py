8bee8d6a7b2a1c8f2955fcf99c2d456472ca5f5b
Pete Peterson <petersonpf@ornl.gov>
2023-07-18 15:32:36 -0400

    First set of command line arguments

--- src/snapred/__main__.py
+++ src/snapred/__main__.py
@@ -1,12 +1,12 @@
 import sys
 
+from snapred import __version__ as snapred_version
 from snapred.meta.Config import Resource
 
 
-def main(args=None):  # noqa: ARG001
-    # ex.resize(700, 700)
-    asciiPath = "ascii.txt"
-    with Resource.open(asciiPath, "r") as asciiArt:
+def _print_text_splash():
+    TXT_FILENAME = "ascii.txt"
+    with Resource.open(TXT_FILENAME, "r") as asciiArt:
         artString = asciiArt.read().split("\n")
         ornlLogo = artString[:33]
         snapRedText = artString[33:]
@@ -14,6 +14,19 @@ def main(args=None):  # noqa: ARG001
             print("\033[38:2:8:{}:{}:{}m{}\033[00m".format(0, 60 + value * 4, value * 2, line))
         for value, line in enumerate(snapRedText):
             print("\033[38:2:0:136:{}:{}m {}\033[00m".format(value * 4 + 51, value * 4 + 46, line))
+
+
+def main(args=None):
+    import argparse
+
+    parser = argparse.ArgumentParser(
+        prog="snapred", description="Data reduction software for SNAP", epilog="https://snapred.readthedocs.io/"
+    )
+    parser.add_argument("-v", "--version", action="version", version=snapred_version)
+    parser.parse_args(args)
+
+    # show the ascii splash screen
+    _print_text_splash()
 
     # start the gui
     from snapred.ui.main import start
