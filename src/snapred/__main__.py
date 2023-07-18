890eb536509d0473639652fe3fd4cf896e571876
Pete Peterson <petersonpf@ornl.gov>
2023-07-18 16:37:49 -0400

    Add options to turn on mantid network services

--- src/snapred/__main__.py
+++ src/snapred/__main__.py
@@ -1,6 +1,7 @@
 import sys
 
 from snapred import __version__ as snapred_version
+from snapred.backend.shims import amend_mantid_config
 from snapred.meta.Config import Resource
 
 
@@ -16,6 +17,16 @@ def _print_text_splash():
             print("\033[38:2:0:136:{}:{}m {}\033[00m".format(value * 4 + 51, value * 4 + 46, line))
 
 
+def _bool_to_mtd_str(arg: bool) -> str:
+    """mantid.kernel.ConfigService does not understand bool, but does understand
+    the strings "0" and "1". This method converts things
+    """
+    if arg:
+        return "1"
+    else:
+        return "0"
+
+
 def main(args=None):
     import argparse
 
@@ -23,15 +34,34 @@ def main(args=None):
         prog="snapred", description="Data reduction software for SNAP", epilog="https://snapred.readthedocs.io/"
     )
     parser.add_argument("-v", "--version", action="version", version=snapred_version)
-    parser.parse_args(args)
+    parser.add_argument("--checkfornewmantid", action="store_true", help="check for new mantid version on startup")
+    parser.add_argument(
+        "--updateinstruments", action="store_true", help="update user's cache of mantid instrument definitions"
+    )
+    parser.add_argument(
+        "--reportusage", action="store_true", help="post telemetry data to mantid usage reporting service"
+    )
+    options = parser.parse_args(args)
+
+    # fix up some of the options for mantid
+    options.checkfornewmantid = _bool_to_mtd_str(options.checkfornewmantid)
+    options.updateinstruments = _bool_to_mtd_str(options.updateinstruments)
+    options.reportusage = _bool_to_mtd_str(options.reportusage)
 
     # show the ascii splash screen
     _print_text_splash()
 
     # start the gui
-    from snapred.ui.main import start
+    with amend_mantid_config(
+        new_config={
+            "CheckMantidVersion.OnStartup": options.checkfornewmantid,
+            "UpdateInstrumentDefinitions.OnStartup": options.updateinstruments,
+            "usagereports.enabled": options.reportusage,
+        }
+    ):
+        from snapred.ui.main import start
 
-    return start()
+        return start(options)
 
 
 if __name__ == "__main__":
