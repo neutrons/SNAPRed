36e396bdd9327a0add9ebc3c35722ea3545043f3
Michael Walsh <walshmm@ornl.gov>
2023-07-19 16:49:36 -0400

    remove logger cleaner from test file to try and resolve write issue during test

--- tests/unit/backend/recipe/test_GenericRecipe.py
+++ tests/unit/backend/recipe/test_GenericRecipe.py
@@ -52,17 +52,3 @@ class TestGenericRecipe(unittest.TestCase):
         self.GenericRecipe.mantidSnapper.DummyAlgo.assert_called_once_with(
             "", Input=self.mock_reductionIngredients.json()
         )
-
-
-# this at teardown removes the loggers, eliminating logger error printouts
-# see https://github.com/pytest-dev/pytest/issues/5502#issuecomment-647157873
-@pytest.fixture(autouse=True)
-def clear_loggers():  # noqa: PT004
-    """Remove handlers from all loggers"""
-    import logging
-
-    loggers = [logging.getLogger()] + list(logging.Logger.manager.loggerDict.values())
-    for logger in loggers:
-        handlers = getattr(logger, "handlers", [])
-        for handler in handlers:
-            logger.removeHandler(handler)
