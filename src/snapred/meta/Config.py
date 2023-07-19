627144895e782246d728194bf0a3ae9b18054d35
Pete Peterson <peterfpeterson@gmail.com>
2023-07-19 12:55:25 -0400

    Config and Resource class changes (#59)
    * Change to not care what checkout directory is named
    
    * Check if file exists rather than using an exception
    
    * Add the ability to refresh the configuration
    
    * Add test for accessing values from Config

--- src/snapred/meta/Config.py
+++ src/snapred/meta/Config.py
@@ -1,4 +1,5 @@
 import importlib.resources as resources
+import logging
 import os
 import sys
 from typing import Any, Dict
@@ -8,40 +9,51 @@ from pydantic.utils import deep_update
 
 from snapred.meta.decorators.Singleton import Singleton
 
-ROOT_MODULE = None
-if os.environ.get("env") != "test":
-    ROOT_MODULE = sys.modules["__main__"].__file__
-else:
-    ROOT_MODULE = sys.modules["conftest"].__file__
 
-if ROOT_MODULE is None:
-    raise Exception("Unable to determine root directory")
+def _find_root_dir():
+    ROOT_MODULE = None
+    if os.environ.get("env") != "test":
+        ROOT_MODULE = sys.modules["__main__"].__file__
+    else:
+        ROOT_MODULE = sys.modules["conftest"].__file__
 
-ROOT_DIR = os.path.dirname(ROOT_MODULE)
+    if ROOT_MODULE is None:
+        raise Exception("Unable to determine root directory")
+
+    return os.path.dirname(ROOT_MODULE)
 
 
 @Singleton
 class _Resource:
-    _packageMode = False
-    _resourcesPath = ROOT_DIR + "/resources/"
+    _packageMode: bool
+    _resourcesPath: str
+    _logger = logging.getLogger("snapred.meta.Config.Resource")
 
     def __init__(self):
-        try:
-            self.open("application.yml", "r")
-        except FileNotFoundError:
-            self._packageMode = True
-            self._resourcesPath = "/resources/"
-        # filename = resource_filename(Requirement.parse("MyProject"),"sample.conf")
-
-    def exists(self, subPath):
+        # where the location of resources are depends on whether or not this is in package mode
+        self._packageMode = not self._existsInPackage("application.yml")
         if self._packageMode:
-            with resources.path("snapred.resources", subPath) as path:
-                return os.path.exists(path)
+            self._logger.debug("In package mode")
+            self._resourcesPath = "/resources/"
+        else:
+            self._logger.debug("Not in package mode")
+            self._resourcesPath = os.path.join(_find_root_dir(), "resources/")
+
+    def _existsInPackage(self, subPath) -> bool:
+        with resources.path("snapred.resources", subPath) as path:
+            return os.path.exists(path)
+
+    def exists(self, subPath) -> bool:
+        if self._packageMode:
+            return self._existsInPackage(subPath)
         else:
             return os.path.exists(self.getPath(subPath))
 
     def getPath(self, subPath):
-        return self._resourcesPath + subPath
+        if subPath.startswith("/"):
+            return os.path.join(self._resourcesPath, subPath[1:])
+        else:
+            return os.path.join(self._resourcesPath, subPath)
 
     def read(self, subPath):
         with self.open(subPath, "r") as file:
@@ -63,16 +75,27 @@ class _Config:
     _config: Dict[str, Any] = {}
 
     def __init__(self):
-        with Resource.open("application.yml", "r") as file:
-            self._config = yaml.safe_load(file)
-            env = os.environ.get("env")
-            if env is None:
-                env = self._config.get("environment", None)
-            if env is not None:
-                with Resource.open("{}.yml".format(env), "r") as file:
-                    envConfig = yaml.safe_load(file)
-                    self._config = deep_update(self._config, envConfig)
-            self._config["environment"] = env
+        # use refresh to do initial load, clearing shouldn't matter
+        self.refresh("application", True)
+        del self._config["environment"]
+
+        # see if user used environment injection to modify what is needed
+        # this will get from the os environment or from the currently loaded one
+        # first case wins
+        env = os.environ.get("env", self._config.get("environment", None))
+        if env is not None:
+            self.refresh(env)
+
+    def refresh(self, env_name: str, clearPrevious: bool = False) -> None:
+        if clearPrevious:
+            self._config.clear()
+
+        with Resource.open(f"{env_name}.yml", "r") as file:
+            envConfig = yaml.safe_load(file)
+            self._config = deep_update(self._config, envConfig)
+
+        # add the name to the config object
+        self._config["environment"] = env_name
 
     # period delimited key lookup
     def __getitem__(self, key):
--- tests/unit/meta/test_config.py
+++ tests/unit/meta/test_config.py
@@ -1,14 +1,19 @@
-# import pytest
-from snapred.meta.Config import Config, Resource
+import pytest
+from snapred.meta.Config import Config, Resource, _find_root_dir
 
 
 def test_environment():
     assert Config["environment"] == "test"
 
 
+def test_find_root_dir():
+    assert _find_root_dir().endswith("/tests")
+
+
 def test_instrument_home():
-    correctPath = "/SNAPRed/tests/resources/SNAP/"
-    assert Config["instrument.home"][-len(correctPath) :] == correctPath
+    # test verifies that the end of the path is correct
+    correctPathEnding = "/tests/resources/SNAP/"
+    assert Config["instrument.home"].endswith(correctPathEnding)
 
 
 def test_resource_exists():
@@ -26,3 +31,17 @@ def test_resource_read():
 def test_resouce_open():
     with Resource.open("application.yml", "r") as file:
         assert file is not None
+
+
+def test_config_accessor():
+    # these values are copied from tests/resources/application.yml
+    assert Config["environment"] == "test"
+    assert Config["instrument.name"] == "SNAP"
+    assert Config["nexus.file.extension"] == ".nxs.h5"
+    assert Config["calibration.file.extension"] == ".json"
+
+    # these should throw KeyError
+    with pytest.raises(KeyError):
+        assert Config["garbage"]
+    with pytest.raises(KeyError):
+        assert Config["orchestration.garbage"]
