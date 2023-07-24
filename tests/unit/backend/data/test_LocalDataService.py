6f664c8ffb6c2c622dbe94a309c912d6bc652799
Pete Peterson <peterfpeterson@gmail.com>
2023-07-24 12:38:52 -0400

    Add test for starting GUI in CI (#63)
    * Split out gui code into gui sub-project
    
    * Make so the application can error out
    
    * First set of command line arguments
    
    * Add options to turn on mantid network services
    
    * Add command line option for starting GUI on build server
    
    * Add step for starting GUI in CI
    
    * Split out gui startup test to separate step
    
    * Add documenation on how to override application settings
    
    * Add missing double quote
    
    * Try installing snapred before running it
    
    * Use test settings file
    
    * Change environment injection method
    
    * Create pretend file for gui to find during startup test
    
    * Copy an existing instrument parameters
    
    * Add test of broken configuration
    
    * Add arg for testing and improve error messages
    
    * Change validatepaths parameter to application setting
    
    * Turn on path validation
    
    * Make custom settings for headcheck
    
    * Add some unit tests for main
    
    * Add test for amend config
    
    * Change to ternary operator

--- tests/unit/backend/data/test_LocalDataService.py
+++ tests/unit/backend/data/test_LocalDataService.py
@@ -4,6 +4,7 @@ import shutil
 import unittest.mock as mock
 from typing import List
 
+import pytest
 from pydantic import parse_raw_as
 from snapred.meta.Config import Resource
 
@@ -381,3 +382,13 @@ with mock.patch.dict("sys.modules", {"mantid.api": mock.Mock(), "h5py": mock.Moc
         actual = localDataService.initializeState("123", "test")
         actual.creationDate = testCalibrationData.creationDate
         assert actual == testCalibrationData
+
+    def test_badPaths():
+        """This verifies that a broken configuration (from production) can't find all of the files"""
+        # get a handle on the service
+        service = LocalDataService()
+        service.verifyPaths = True  # override test setting
+        with pytest.raises(FileNotFoundError):
+            service.readInstrumentConfig()
+
+        service.verifyPaths = False  # put the setting back
