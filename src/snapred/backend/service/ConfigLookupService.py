a74ee8d14d5264f1ccbaf2cae395f4000ab19320
Michael Walsh <walshmm@ornl.gov>
2023-08-02 11:15:34 -0400

    fix various bugs found by Malcolm during testing

--- src/snapred/backend/service/ConfigLookupService.py
+++ src/snapred/backend/service/ConfigLookupService.py
@@ -28,5 +28,5 @@ class ConfigLookupService(Service):
     def getConfigs(self, runs: List[RunConfig]):
         data = {}
         for run in runs:
-            data[run.runId] = self.dataFactoryService.getReductionState(run.runId)
+            data[run.runId] = self.dataFactoryService.getReductionState(run.runNumber)
         return data
