67d085bd2551274c10a69e502d0ffc0eba89f4c0
darshdinger <ddarsh5544@gmail.com>
2023-08-04 08:52:16 -0400

    Added worker flow.

--- src/snapred/ui/presenter/InitializeCalibrationPresenter.py
+++ src/snapred/ui/presenter/InitializeCalibrationPresenter.py
@@ -1,13 +1,18 @@
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QVBoxLayout, QLabel, QWidget, QLineEdit
+from PyQt5.QtWidgets import QLabel, QVBoxLayout, QWidget
 
 from snapred.backend.api.InterfaceController import InterfaceController
-from snapred.backend.dao.StateConfig import StateConfig
-from snapred.backend.dao.SNAPResponse import SNAPResponse
 from snapred.backend.dao.SNAPRequest import SNAPRequest
+from snapred.backend.dao.SNAPResponse import SNAPResponse
+from snapred.backend.dao.StateConfig import StateConfig
+from snapred.ui.threading.worker_pool import WorkerPool
 from snapred.ui.view.PromptUserforCalibrationInputView import PromptUserforCalibrationInputView
 
+
 class CalibrationCheck(object):
+    worker_pool = WorkerPool()
+    interfaceController = InterfaceController()
+
     def __init__(self, view):
         self.view = view
 
@@ -19,40 +24,81 @@ class CalibrationCheck(object):
         vbox.addWidget(label)
         vbox.addStretch()
         win.setLayout(vbox)
-        self.view.layout.addWidget(win)
+        self.view.layout().addWidget(win)
 
-    def handleButtonClicked(self, response: SNAPResponse):
+    def handleButtonClicked(self):
         self.view.beginFlowButton.setEnabled(False)
 
         # Check to see if data exists
         runNumber = self.view.getRunNumber()
         runNumber_str = str(runNumber)
         dataCheckRequest = SNAPRequest(path="/calibration/checkDataExists", payload=runNumber_str)
-        response = InterfaceController.executeRequest(dataCheckRequest)
 
+        self.worker = self.worker_pool.createWorker(
+            target=self.interfaceController.executeRequest, args=(dataCheckRequest)
+        )
+        self.worker.result.connect(self.handleDataCheckResult)
+
+        self.worker_pool.submitWorker(self.worker)
+
+    def handleDataCheckResult(self, response: SNAPResponse):
         if response.responseCode != 200:
             self._labelView("Error, data doesn't exist")
             return
 
+        runNumber_str = str(self.view.getRunNumber())
         stateCheckRequest = SNAPRequest(path="/calibration/hasState", payload=runNumber_str)
-        response = InterfaceController.executeRequest(stateCheckRequest)
 
+        self.worker = self.worker_pool.createWorker(
+            target=self.interfaceController.executeRequest, args=(stateCheckRequest)
+        )
+        self.worker.result.connect(self.handleStateCheckResult)
+
+        self.worker_pool.submitWorker(self.worker)
+
+    def handleStateCheckResult(self, response: SNAPResponse):
         if response.responseCode != 200:
             self._spawnStateCreationWorkflow()
+            return
 
         groupingFile = str(StateConfig.focusGroups.definition)
-
         pixelGroupingParametersRequest = SNAPRequest("/calibration/calculatePixelGroupingParameters", groupingFile)
 
-        if pixelGroupingParametersRequest:
+        self.worker = self.worker_pool.createWorker(
+            target=self.interfaceController.executeRequest, args=(pixelGroupingParametersRequest)
+        )
+        self.worker.result.connect(self.handlePixelGroupingResult)
+
+        self.worker_pool.submitWorker(self.worker)
+
+    def handlePixelGroupingResult(self, response: SNAPResponse):
+        if response.responseCode == 200:
             self._labelView("Ready to Calibrate!")
 
     def _spawnStateCreationWorkflow(self):
-        from snapred.ui.widget.WorkflowNode import startWorkflow, continueWorkflow, finalizeWorkflow
+        from snapred.ui.widget.WorkflowNode import continueWorkflow, finalizeWorkflow, startWorkflow
 
         promptView = PromptUserforCalibrationInputView()
+
+        def push_data_to_interface_controller(workflow):
+            run_number = promptView.getRunNumber()
+            state_name = promptView.getName()
+
+            payload = {"runNumber": run_number, "stateName": state_name}
+
+            request = SNAPRequest(path="/path/to/initialize/state", payload=payload)
+
+            self.worker = self.worker_pool.createWorker(target=self.interfaceController.executeRequest, args=(request,))
+
+            def handle_response(response):
+                pass
+
+            self.worker.result.connect(handle_response)
+
+            self.worker_pool.submitWorker(self.worker)
+
         Start = startWorkflow(None, promptView)
-        Continue = continueWorkflow(Start, lambda workflow: """push data to interfacecontroller""", promptView)
+        Continue = continueWorkflow(Start, push_data_to_interface_controller, promptView)
         Finish = finalizeWorkflow(Continue)
 
         Finish.presenter.show()
