8dc5618f6b80231a8acb2d5f3e64a44f52fe3927
darshdinger <ddarsh5544@gmail.com>
2023-08-15 15:01:40 -0400

    Fixed the tests, hopefully now this will be okay. Pre-commit doesn't work because ruff fails due to using qtbot but it isn't called so ruff fails.

--- tests/unit/ui/presenter/test_InitializeCalibrationPresenter.py
+++ tests/unit/ui/presenter/test_InitializeCalibrationPresenter.py
@@ -1,6 +1,7 @@
 from unittest.mock import Mock, patch
 
 import pytest
+from qtpy import QtCore
 from snapred.backend.dao.SNAPRequest import SNAPRequest
 from snapred.backend.dao.SNAPResponse import SNAPResponse
 from snapred.ui.presenter.InitializeCalibrationPresenter import CalibrationCheck
@@ -17,7 +18,7 @@ def mock_View():
     return view
 
 
-def test_labelView(mock_View):
+def test_labelView(mock_View, qtbot):
     calibrationCheck = CalibrationCheck(mock_View)
     test_text = "Test"
     calibrationCheck._labelView(test_text)
@@ -25,7 +26,7 @@ def test_labelView(mock_View):
     mock_View.layout().addWidget.assert_called()
 
 
-def test_handleButtonClicked(mock_View):
+def test_handleButtonClicked(mock_View, qtbot):
     with patch(
         "snapred.ui.presenter.InitializeCalibrationPresenter.CalibrationCheck.worker_pool.createWorker",
         return_value=Mock(),
@@ -39,7 +40,7 @@ def test_handleButtonClicked(mock_View):
         )
 
 
-def test_handleDataCheckResult(mock_View):
+def test_handleDataCheckResult(mock_View, qtbot):
     calibrationCheck = CalibrationCheck(mock_View)
     mock_response = Mock()
     mock_response.responseCode = 404
@@ -53,7 +54,7 @@ def test_handleDataCheckResult(mock_View):
     mock_labelView.assert_called_with("Error, data doesn't exist")
 
 
-def test_handleStateCheckResult(mock_View):
+def test_handleStateCheckResult(mock_View, qtbot):
     calibration_check = CalibrationCheck(mock_View)
     mock_response = Mock()
     mock_response.responseCode = 404
@@ -63,7 +64,7 @@ def test_handleStateCheckResult(mock_View):
         mock_spawn_workflow.assert_called_once()
 
 
-def test_handlePixelGroupingResult(mock_View):
+def test_handlePixelGroupingResult(mock_View, qtbot):
     calibrationCheck = CalibrationCheck(mock_View)
     mock_response = Mock()
     mock_response.responseCode = 200
--- tests/unit/ui/view/test_InitializeCalibrationCheckView.py
+++ tests/unit/ui/view/test_InitializeCalibrationCheckView.py
@@ -1,33 +1,24 @@
-import sys
-
-from PyQt5.QtCore import *
-from PyQt5.QtGui import *
-from PyQt5.QtWidgets import QApplication, QComboBox
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QComboBox
 from snapred.ui.view.InitializeCalibrationCheckView import CalibrationMenu
 
-app = QApplication(sys.argv)
 
-
-def test_run_number_field():
+def test_run_number_field(qtbot):
     menu = CalibrationMenu(None)
-    menu.show()
+    qtbot.addWidget(menu)
 
     assert menu.getRunNumber() == ""
 
     test_value = "1234"
-    menu.runNumberField.setText(test_value)
-    assert menu.getRunNumber() == test_value
-
-    test_value = "5678"
-    menu.runNumberField.setText(test_value)
+    qtbot.keyClicks(menu.runNumberField, test_value)
     assert menu.getRunNumber() == test_value
 
     sample_dropdown = menu.findChild(QComboBox, "sampleDropdown")
+    qtbot.mouseClick(sample_dropdown, Qt.LeftButton)
     sample_dropdown.setCurrentIndex(1)
     assert sample_dropdown.currentText() != "Select Sample"
 
     grouping_file_dropdown = menu.findChild(QComboBox, "groupingFileDropdown")
+    qtbot.mouseClick(grouping_file_dropdown, Qt.LeftButton)
     grouping_file_dropdown.setCurrentIndex(1)
     assert grouping_file_dropdown.currentText() != "Select Grouping File"
-
-    menu.close()
