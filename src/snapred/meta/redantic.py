485a5d62840434e7447f2329cd3f41d051ca3c41
Michael Walsh <walshmm@ornl.gov>
2023-08-22 11:35:34 -0400

    update json writes to produce legible files

--- src/snapred/meta/redantic.py
+++ src/snapred/meta/redantic.py
@@ -1,4 +1,29 @@
-def list_to_raw(baseModelList):
-    import json
+import json
 
+
+def list_to_raw(baseModelList):
     return json.dumps([baseModel.dict() for baseModel in baseModelList])
+
+
+def list_to_raw_pretty(baseModelList):
+    return json.dumps([baseModel.dict() for baseModel in baseModelList], indent=4)
+
+
+def write_model(baseModel, path):
+    with open(path, "w") as f:
+        f.write(baseModel.json())
+
+
+def write_model_pretty(baseModel, path):
+    with open(path, "w") as f:
+        f.write(baseModel.json(indent=4))
+
+
+def write_model_list(baseModelList, path):
+    with open(path, "w") as f:
+        f.write(list_to_raw(baseModelList))
+
+
+def write_model_list_pretty(baseModelList, path):
+    with open(path, "w") as f:
+        f.write(list_to_raw_pretty(baseModelList))
