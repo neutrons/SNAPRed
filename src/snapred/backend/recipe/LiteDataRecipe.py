f5a8632d51eadbf25bc9a7858f4264966397ec1a
darshdinger <ddarsh5544@gmail.com>
2023-08-31 08:26:07 -0400

    Added the service.

---
+++ src/snapred/backend/recipe/LiteDataRecipe.py
@@ -0,0 +1,33 @@
+from typing import Any, Dict, List
+
+from mantid.api import AlgorithmManager
+
+from snapred.backend.log.logger import snapredLogger
+from snapred.backend.recipe.algorithm.LiteDataCreationAlgo import (
+    name as LiteDataCreationAlgo,
+)
+from snapred.meta.decorators.Singleton import Singleton
+
+logger = snapredLogger.getLogger(__name__)
+
+
+@Singleton
+class LiteDataRecipe:
+    liteDataCreationAlgoName: str = LiteDataCreationAlgo
+
+    def __init__(self):
+        pass
+
+    def exectuteRecipe(self, InputWorkspace: str):
+        logger.info("Executing LiteDataRecipe")
+        data: Dict[str, Any] = {}
+
+        algo = AlgorithmManager.create(self.liteDataCreationAlgoName)
+        algo.setProperty("InputWorkspace", InputWorkspace)
+        try:
+            data["result"] = algo.execute()
+        except RuntimeError as e:
+            errorString = str(e)
+            raise Exception(errorString.split("\n")[0])
+        logger.info("LiteDataCreationAlgo complete")
+        return data
