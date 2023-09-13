5db4445eaccefa1809aeaabaa3a9bc8aaa19ed62
Michael Walsh <walshmm@ornl.gov>
2023-09-13 15:14:06 -0400

    added a cis test script for smoothdataexcludingpeaks

---
+++ tests/cis_tests/smooth_data_excluding_peaks.py
@@ -0,0 +1,44 @@
+from mantid.simpleapi import *
+import matplotlib.pyplot as plt
+import numpy as np
+import json
+from snapred.backend.dao.ingredients import SmoothDataExcludingPeaksIngredients
+from snapred.backend.recipe.algorithm.DetectorPeakPredictor import SmoothDataExcludingPeaks
+from snapred.backend.data.DataFactoryService import DataFactoryService
+from snapred.backend.service.CrystallographicInfoService import CrystallographicInfoService
+from snapred.backend.service.CalibrationService import CalibrationService
+from snapred.backend.log.logger import snapredLogger
+snapredLogger._level = 20
+
+#diffraction calibration imports
+from typing import List
+
+#User inputs ###########################
+runNumber = ‘58882’#58409'
+cifPath = ‘/SNS/SNAP/shared/Calibration/CalibrantSamples/Silicon_NIST_640d.cif’
+groupingFile = ‘/SNS/SNAP/shared/Calibration/Powder/PixelGroupingDefinitions/SNAPFocGrp_Column.lite.xml’
+#######################################
+
+
+dataFactoryService=DataFactoryService()
+calibrationService = CalibrationService()
+pixelGroupingParameters = calibrationService.retrievePixelGroupingParams(runNumber)
+calibration = dataFactoryService.getCalibrationState(runNumber)
+# focusGroups = reductionIngredients.reductionState.stateConfig.focusGroups
+
+reductionIngredients = dataFactoryService.getReductionIngredients(runNumber)
+ipts = reductionIngredients.runConfig.IPTS
+rawDataPath = ipts + "shared/lite/SNAP_{}.lite.nxs.h5".format(reductionIngredients.runConfig.runNumber)
+instrumentState = calibration.instrumentState
+crystalInfoDict = CrystallographicInfoService().ingest(cifPath)
+instrumentState.pixelGroupingInstrumentParameters = pixelGroupingParameters[0]
+ws = "raw_data"
+LoadEventNexus(Filename=rawDataPath, OutputWorkspace=ws)
+
+ingredients = SmoothDataExcludingPeaksIngredients(instrumentState=instrumentState, crystalInfo=crystalInfoDict[‘crystalInfo’], smoothingParameter=0.05)
+
+algo = SmoothDataExcludingPeaks()
+algo.initialize()
+algo.setProperty(“Ingredients”, ingredients.json())
+algo.setProperty(“InputWorkspace”, ws)
+algo.execute()
