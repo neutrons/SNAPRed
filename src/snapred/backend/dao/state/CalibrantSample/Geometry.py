c0e57e77adae16ef642963fc1243cd53f9856949
Daniel Caballero <dlcaballero16@gmail.com>
2023-09-27 13:34:04 -0400

    Updated CalibrantSample classes to match SetSample Geometry and Material classes had members updated to match what is being used in mantid's SetSample() function

--- src/snapred/backend/dao/state/CalibrantSample/Geometry.py
+++ src/snapred/backend/dao/state/CalibrantSample/Geometry.py
@@ -1,26 +1,26 @@
-from typing import Optional
+from typing import List, Optional
 
 from pydantic import BaseModel, root_validator
 
 
 class Geometry(BaseModel):
     """Class to hold Geometry data for Calibrant Samples
-    form: string must be 'cylinder' or 'sphere'
-    radius: single float (cm)
-    illuminated height: single float (cm)
-    total height: single float (cm) (not used for sphere)"""
+    Shape: string must be 'Cylinder' or 'Sphere'
+    Radius: single float (cm)
+    Height: single float (cm) (not used for sphere)
+    Center: list of three floats (cm)"""
 
-    form: str
-    radius: float
-    illuminated_height: float
-    total_height: Optional[float] = None
+    Shape: str
+    Radius: float
+    Height: Optional[float]
+    Center: List[float]
 
     @root_validator(pre=True, allow_reuse=True)
     def validate_form(cls, v):
-        form, total_height = v.get("form").strip(), v.get("total_height")
-        if form != "cylinder" and form != "sphere":
-            raise ValueError('form must be "cylinder" or "sphere"')
-        if form == "sphere" and total_height is not None:
+        Shape, Height = v.get("Shape").strip(), v.get("Height")
+        if Shape != "Cylinder" and Shape != "Sphere":
+            raise ValueError('form must be "Cylinder" or "Sphere"')
+        if Shape == "Sphere" and Height is not None:
             v.set("total_height", None)
-            raise Warning("total height is not used with a sphere")
+            raise Warning("Height is not used with a sphere")
         return v
--- src/snapred/backend/dao/state/CalibrantSample/Material.py
+++ src/snapred/backend/dao/state/CalibrantSample/Material.py
@@ -3,27 +3,18 @@ from pydantic import BaseModel, validator
 
 class Material(BaseModel):
     """Class to hold Material information relevant to Calibrant Samples
-    microstructure: str must be 'poly-crystal' or 'single-crystal'
-    packing-fraction: float, [0.,1.]
-    mass density: float (g/cm^3)
-    chemical composition: string following mantid
+    PackingFraction: float, [0.,1.]
+    MassDensity: float (g/cm^3)
+    ChemicalFormula: string following mantid
 
             convention: https://docs.mantidproject.org/nightly/concepts/Materials.html#id3"""
 
-    microstructure: str
-    packing_fraction: float
-    mass_density: float
-    chemical_composition: str
+    PackingFraction: float
+    MassDensity: float
+    ChemicalFormula: str
 
-    @validator("microstructure", allow_reuse=True)
-    def validate_microstructure(cls, v):
-        v = v.strip()
-        if v != "poly-crystal" and v != "single-crystal":
-            raise ValueError("microstructure must be 'poly-crystal' or 'single-crystal'")
-        return v
-
-    @validator("packing_fraction", allow_reuse=True)
-    def validate_packing_fraction(cls, v):
+    @validator("PackingFraction", allow_reuse=True)
+    def validate_PackingFraction(cls, v):
         if v < 0 or v > 1:
-            raise ValueError("packing fraction must be a value in the range [0, 1]")
+            raise ValueError("PackingFraction must be a value in the range [0, 1]")
         return v
