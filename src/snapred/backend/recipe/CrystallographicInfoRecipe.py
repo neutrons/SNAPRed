c8919775afb18563e894c0c9fd58e5f1efd84619
Darsh <ddarsh5544@gmail.com>
2023-10-09 09:02:09 -0400

    Changes to CrystallographicInfoRecipe and DetectorPeakPredictor

--- src/snapred/backend/recipe/CrystallographicInfoRecipe.py
+++ src/snapred/backend/recipe/CrystallographicInfoRecipe.py
@@ -28,22 +28,8 @@ class CrystallographicInfoRecipe:
         try:
             data["result"] = algo.execute()
             data["crystalInfo"] = CrystallographicInfo.parse_raw(algo.getProperty("crystalInfo").value)
-            data["fSquaredThreshold"] = self.findFSquaredThreshold(data["crystalInfo"])
         except RuntimeError as e:
             errorString = str(e)
             raise Exception(errorString.split("\n")[0])
         logger.info("Finished ingesting crystal info: %s" % cifPath)
         return data
-
-    def findFSquaredThreshold(self, xtal: CrystallographicInfo):
-        # set a threshold for weak peaks in the spectrum
-        # this uses the median of lowest 1% of intensities
-
-        # intensity is fsq times multiplicities
-        I0 = [ff * mm * (dd**4) for ff, mm, dd in zip(xtal.fSquared, xtal.multiplicities, xtal.dSpacing)]
-        I0.sort()
-
-        # take lowest one percent
-        numPeaks = len(xtal.fSquared)
-        lowest = max(1, round(numPeaks / 100)) - 1
-        return I0[int(lowest / 2)]
