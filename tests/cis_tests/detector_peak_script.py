536a7999ec22cb15436e2704ac469b48d9f46aac
Darsh <ddarsh5544@gmail.com>
2023-10-10 11:52:32 -0400

    Updated detector_peak_script. Reese provided the refactored code and asked me to include it in this PR.

--- tests/cis_tests/detector_peak_script.py
+++ tests/cis_tests/detector_peak_script.py
@@ -15,6 +15,7 @@ from snapred.backend.data.DataFactoryService import DataFactoryService
 from snapred.backend.service.CrystallographicInfoService import CrystallographicInfoService
 from snapred.backend.service.CalibrationService import CalibrationService
 from snapred.backend.log.logger import snapredLogger
+from snapred.meta.Config import Config
 from snapred.meta.redantic import list_to_raw_pretty
 
 snapredLogger._level = 20
@@ -24,25 +25,31 @@ snapredLogger._level = 20
 runNumber = '58882'#58409'
 cifPath = '/SNS/SNAP/shared/Calibration/CalibrantSamples/Silicon_NIST_640d.cif'
 groupingFile = '/SNS/SNAP/shared/Calibration/Powder/PixelGroupingDefinitions/SNAPFocGrp_Column.lite.xml'
+peakFractionalThreshold = 0.01
+isLite = False
 
+# SET TO TRUE TO STOP WASHING DISHES
+Config._config['cis_mode'] = False
 #######################################
 
 dataFactoryService=DataFactoryService()
-calibrationService = CalibrationService()
-
-pixelGroupingParameters = calibrationService.retrievePixelGroupingParams(runNumber)
-
+runConfig = dataFactoryService.getRunConfig(runNumber)
+focusGroup = dataFactoryService.getFocusGroups(runNumber)[0] #
 calibration = dataFactoryService.getCalibrationState(runNumber)
-# focusGroups = reductionIngredients.reductionState.stateConfig.focusGroups
 instrumentState = calibration.instrumentState
-crystalInfoDict = CrystallographicInfoService().ingest(cifPath)
+calPath = instrumentState.instrumentConfig.calibrationDirectory
+
+calibrationService = CalibrationService()
+pixelGroupingParameters = calibrationService.retrievePixelGroupingParams(runNumber)
 instrumentState.pixelGroupingInstrumentParameters = pixelGroupingParameters[0]
+
+crystalInfoDict = CrystallographicInfoService().ingest(cifPath)
 
 detectorAlgo = DetectorPeakPredictor()
 detectorAlgo.initialize()
 detectorAlgo.setProperty("InstrumentState", instrumentState.json())
 detectorAlgo.setProperty("CrystalInfo", crystalInfoDict['crystalInfo'].json())
-detectorAlgo.setProperty("PeakIntensityFractionThreshold", 0.01)
+detectorAlgo.setProperty("PeakIntensityFractionThreshold", peakFractionalThreshold)
 detectorAlgo.execute()
 
 peakList = json.loads(detectorAlgo.getProperty("DetectorPeaks").value)
@@ -66,25 +73,48 @@ for i,group in enumerate(peakList):
 
 
 ################################################################
-# Pause here to run diffraction calibration on input workspace
+# Plot the found peaks on focused data
 ################################################################
+from snapred.backend.recipe.DiffractionCalibrationRecipe import DiffractionCalibrationRecipe
+from snapred.backend.dao.ingredients import DiffractionCalibrationIngredients
+ingredients = DiffractionCalibrationIngredients(
+    runConfig=runConfig,
+    instrumentState=instrumentState,
+    focusGroup=focusGroup,
+    groupedPeakLists=peakList,
+    calPath=calPath,
+    convergenceThreshold=0.01,
+)
+rx = DiffractionCalibrationRecipe()
+res = rx.executeRecipe(ingredients) # <<-- WILL FAIL AT SAVE STEP -- IGNORE
 
+
+ConvertUnits(
+    InputWorkspace = f'_TOF_{runNumber}_diffoc',
+    OutputWorkspace = f'_DSP_{runNumber}_diffoc',
+    Target="dSpacing",
+)
+Rebin(
+    InputWorkspace = f'_DSP_{runNumber}_diffoc',
+    OutputWorkspace = f'_DSP_{runNumber}_diffoc',
+    Params = (0.01),
+)
 
 #do the plotting
-tableName = 'peakProperties1'
-fig, ax = plt.subplots(subplot_kw={'projection':'mantid'})
-ax.plot(mtd['_DSP_58882_raw'], wkspIndex=0)# plot the initial guess with black line
-ax.vlines(mtd[tableName].column(1), ymin=1e6, ymax=1e8, color='red')
-ax.vlines(mtd[tableName].column(2), ymin=1e6, ymax=1e8, color='orange')
-ax.vlines(mtd[tableName].column(3), ymin=1e6, ymax=1e8, color='orange')
-ax.legend() # show the legend
-fig.show()
-
+for i,group in enumerate(peakList):
+    tableName = f'peakProperties{i+1}'
+    fig, ax = plt.subplots(subplot_kw={'projection':'mantid'})
+    ax.plot(mtd[f'_DSP_{runNumber}_diffoc'], wkspIndex=i)# plot the initial guess with black line
+    ax.vlines(mtd[tableName].column(1), ymin=1e6, ymax=1e8, color='red')
+    ax.vlines(mtd[tableName].column(2), ymin=1e6, ymax=1e8, color='orange')
+    ax.vlines(mtd[tableName].column(3), ymin=1e6, ymax=1e8, color='orange')
+    ax.legend() # show the legend
+    fig.show()
 
 #########################################################
 # TRY CHANGING PEAK TAIL MAX AND RE_RUN FOR COMPARISON
 #########################################################
-
+assert False
 # using previously found ingredients, change the peakTailCoefficient within instrumentstate
 instrumentState.peakTailCoefficient = 10
 
@@ -115,11 +145,12 @@ for i,group in enumerate(peakList):
         })
 
 #do the plotting
-tableName = 'peakProperties1_after'
-fig, ax = plt.subplots(subplot_kw={'projection':'mantid'})
-ax.plot(mtd['_DSP_58882_raw'], wkspIndex=0)# plot the initial guess with black line
-ax.vlines(mtd[tableName].column(1), ymin=1e6, ymax=1e8, color='red')
-ax.vlines(mtd[tableName].column(2), ymin=1e6, ymax=1e8, color='orange')
-ax.vlines(mtd[tableName].column(3), ymin=1e6, ymax=1e8, color='orange')
-ax.legend() # show the legend
-fig.show()
+for i,group in enumerate(peakList):
+    tableName = f'peakProperties{i+1}_after'
+    fig, ax = plt.subplots(subplot_kw={'projection':'mantid'})
+    ax.plot(mtd[f'_DSP_{runNumber}_diffoc'], wkspIndex=i)# plot the initial guess with black line
+    ax.vlines(mtd[tableName].column(1), ymin=1e6, ymax=1e8, color='red')
+    ax.vlines(mtd[tableName].column(2), ymin=1e6, ymax=1e8, color='orange')
+    ax.vlines(mtd[tableName].column(3), ymin=1e6, ymax=1e8, color='orange')
+    ax.legend() # show the legend
+    fig.show()
