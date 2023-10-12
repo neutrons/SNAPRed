6543e260637a7dddea6ec53c243e52abdf3e7441
rboston628 <bostonsr@ornl.gov>
2023-10-12 09:30:50 -0400

    change calibrant sample script

--- tests/cis_tests/calibrant_samples_script.py
+++ tests/cis_tests/calibrant_samples_script.py
@@ -1,4 +1,4 @@
-from mantid.simpleapi import *
+from mantid.simpleapi import CreateWorkspace, SetSample
 from mantid.geometry import CrystalStructure
 from snapred.backend.dao.state.CalibrantSample.CalibrantSamples import CalibrantSamples
 from snapred.backend.dao.state.CalibrantSample.Crystallography import Crystallography
@@ -8,7 +8,7 @@ from snapred.backend.dao.state.CalibrantSample.Atom import Atom
 
 mat = Material(chemicalFormula="(Li7)2-C-H4-N-Cl6", massDensity=4.4, packingFraction=0.9)
 geo = Geometry(shape="Cylinder", radius=0.1, height=3.6, center=[0.0, 0.0, 0.0])
-atom = Atom(atom_type="Si", atom_coordinates=[0.125, 0.125, 0.125], site_occupation_factor=1.0)
+atom = Atom(symbol="Si", coordinates=[0.125, 0.125, 0.125], siteOccupationFactor=1.0)
 crystal = Crystallography(
     cifFile="/SNS/SNAP/shared/Calibration/CalibrantSamples/Silicon_NIST_640d.cif",
     spaceGroup="F d -3 m",
