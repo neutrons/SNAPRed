eadd74b294b41b99bc421dd4126ab10d9101b740
Darsh <ddarsh5544@gmail.com>
2023-10-13 11:03:22 -0400

    Updated Normalization Calibration Request

--- src/snapred/backend/dao/request/NormalizationCalibrationRequest.py
+++ src/snapred/backend/dao/request/NormalizationCalibrationRequest.py
@@ -1,8 +1,14 @@
 from pydantic import BaseModel
 
 from snapred.backend.dao.RunConfig import RunConfig
+from snapred.backend.dao.ingredients.ReductionIngredients import ReductionIngredients
+from snapred.backend.dao.ingredients.SmoothDataExcludingPeaksIngredients import SmoothDataExcludingPeaksIngredients
 
 
 class NormalizationCalibrationRequest(BaseModel):
     runNumber: RunConfig
     emptyRunNumber: RunConfig
+    calibrantPath: str
+    smoothDataExcludingPeaksIngredients: SmoothDataExcludingPeaksIngredients
+    reductionIngredients: ReductionIngredients
+    
