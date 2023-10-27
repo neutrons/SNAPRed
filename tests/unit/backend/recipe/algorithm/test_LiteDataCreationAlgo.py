8f1790b57957286bc4b7568d8691518e37f0d4c3
Darsh <ddarsh5544@gmail.com>
2023-10-27 12:41:15 -0400

    Updated tests.

--- tests/unit/backend/recipe/algorithm/test_LiteDataCreationAlgo.py
+++ tests/unit/backend/recipe/algorithm/test_LiteDataCreationAlgo.py
@@ -1,5 +1,6 @@
 import os
 import unittest.mock as mock
+from unittest.mock import patch
 
 import numpy as np
 import pytest
@@ -26,41 +27,6 @@ def _setup_teardown():
             DeleteWorkspace(workspace)
         except ValueError:
             print(f"Workspace {workspace} doesn't exist!")
-
-
-@pytest.mark.mount_snap()
-@pytest.mark.skipif(not HAVE_MOUNT_SNAP, reason="Mount SNAP not available")
-def test_LiteDataCreationAlgo_basic_functionality():
-    testWorkspaceFile = "/SNS/SNAP/IPTS-26687/nexus/SNAP_51877.nxs.h5"
-    test_ws_name = "test_ws"
-    Load(Filename=testWorkspaceFile, OutputWorkspace=test_ws_name)
-
-    assert test_ws_name in mtd.getObjectNames()
-
-    output_ws_name = "output_ws"
-    liteDataCreationAlgo = LiteDataCreationAlgo()
-    liteDataCreationAlgo.initialize()
-    liteDataCreationAlgo.setProperty("InputWorkspace", test_ws_name)
-    liteDataCreationAlgo.setProperty("OutputWorkspace", output_ws_name)
-    result = liteDataCreationAlgo.execute()
-
-    assert result
-    assert output_ws_name in mtd.getObjectNames()
-    output_ws = mtd[output_ws_name]
-
-    original_ws = mtd[test_ws_name]
-    orig_specs = []
-    modified_specs = []
-    for idx in range(original_ws.getNumberHistograms()):
-        orig_spec = original_ws.getSpectrum(idx)
-        orig_specs.append(orig_spec)
-        modified_spec = output_ws.getSpectrum(idx)
-        modified_specs.append(modified_spec)
-
-    assert len(orig_specs) == len(modified_specs)
-
-    DeleteWorkspace(output_ws_name)
-
 
 @pytest.mark.mount_snap()
 @pytest.mark.skipif(not HAVE_MOUNT_SNAP, reason="Mount SNAP not available")
--- tests/unit/backend/service/test_LiteDataService.py
+++ tests/unit/backend/service/test_LiteDataService.py
@@ -1,23 +1,29 @@
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, MagicMock
 
 from snapred.backend.dao.RunConfig import RunConfig
 
 
 class TestLiteDataService(unittest.TestCase):
     @patch("snapred.backend.data.DataFactoryService.DataFactoryService", autospec=True)
-    @patch("snapred.backend.recipe.GenericRecipe.GenericRecipe.executeRecipe")
+    @patch("snapred.backend.recipe.GenericRecipe.LiteDataRecipe.executeRecipe")
     def test_reduceLiteData_calls_executeRecipe_with_correct_arguments(
         self, mock_executeRecipe, mock_dataFactoryService  # noqa: ARG002
     ):
+        # Set the mock return value
         mock_executeRecipe.return_value = {}
 
+        # Create a mock run configuration
         mock_runConfig = RunConfig(runNumber="12345")
 
+        # Import the service to test
         from snapred.backend.service.LiteDataService import LiteDataService
 
+        # Instantiate the service
         liteDataService = LiteDataService()
 
+        # Call the method to test
         liteDataService.reduceLiteData([mock_runConfig])
 
-        mock_executeRecipe.assert_called_with(inputWorkspace="SNAP_12345.nxs")
+        # Assert the method was called with the correct arguments
+        mock_executeRecipe.assert_called_with(inputWorkspace="SNAP_12345.nxs", runNumber="12345")
