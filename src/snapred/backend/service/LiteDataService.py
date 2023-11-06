d257d26dca6341e204570370e075f2125f8cd59d
Darsh <ddarsh5544@gmail.com>
2023-11-06 14:49:18 -0500

    Updates to Service

--- src/snapred/backend/service/LiteDataService.py
+++ src/snapred/backend/service/LiteDataService.py
@@ -29,7 +29,7 @@ class LiteDataService(Service):
         for run in runs:
             inputWorkspace = "SNAP_" + str(run.runNumber) + ".nxs"
             try:
-                data[str(run.runNumber)] = Recipe().executeRecipe(inputWorkspace=inputWorkspace, Run=run)
+                data[str(run.runNumber)] = Recipe().executeRecipe(inputWorkspace=inputWorkspace, Run=run, isLite=True)
             except Exception as e:
                 raise e
         return data
