d3c43331236e533f32ac1032793a1e2c3caeaaef
Darsh <ddarsh5544@gmail.com>
2023-11-17 09:26:11 -0500

    Added algo and cis_test

---
+++ src/snapred/backend/recipe/algorithm/CalibrationNormalizationAlgo.py
@@ -0,0 +1,162 @@
+import json
+
+from mantid.api import (
+    AlgorithmFactory,
+    ITableWorkspaceProperty,
+    MatrixWorkspaceProperty,
+    PropertyMode,
+    PythonAlgorithm,
+    mtd,
+)
+from mantid.kernel import Direction
+
+from snapred.backend.dao.ingredients.NormalizationCalibrationIngredients import (
+    NormalizationCalibrationIngredients as Ingredients,
+)
+from snapred.backend.recipe.algorithm.LiteDataCreationAlgo import LiteDataCreationAlgo
+from snapred.backend.recipe.algorithm.MantidSnapper import MantidSnapper
+from snapred.backend.recipe.algorithm.RawVanadiumCorrectionAlgorithm import RawVanadiumCorrectionAlgorithm
+from snapred.backend.recipe.algorithm.SmoothDataExcludingPeaksAlgo import SmoothDataExcludingPeaks  # noqa F401
+
+name = "CalibrationNormalizationAlgo"
+
+
+class CalibrationNormalization(PythonAlgorithm):
+    def PyInit(self):
+        # declare properties
+        self.declareProperty(
+            MatrixWorkspaceProperty("InputWorkspace", "ws_in_normalization", Direction.Input, PropertyMode.Optional),
+            doc="Workspace containing the raw vanadium data",
+        )
+        self.declareProperty(
+            MatrixWorkspaceProperty("BackgroundWorkspace", "ws_background", Direction.Input, PropertyMode.Optional),
+            doc="Workspace containing the raw vanadium background data",
+        )
+        self.declareProperty(
+            MatrixWorkspaceProperty("OutputWorkspace", "ws_out_normalization", Direction.Output, PropertyMode.Optional),
+            doc="Workspace containing corrected data; if none given, the InputWorkspace will be overwritten",
+        )
+        self.declareProperty(
+            ITableWorkspaceProperty("CalibrationWorkspace", "", Direction.Input, PropertyMode.Mandatory),
+            doc="Table workspace with calibration data: cols detid, difc, difa, tzero",
+        )
+        self.declareProperty("Ingredients", defaultValue="", direction=Direction.Input)
+        self.declareProperty("FocusWorkspace", defaultValue="", direction=Direction.Output)
+        self.declareProperty("SmoothWorkspace", defaultValue="", direction=Direction.Output)
+        self.setRethrows(True)
+        self.mantidSnapper = MantidSnapper(self, name)
+
+    def chopIngredients(self, ingredients: Ingredients):
+        self.reductionIngredients = ingredients.reductionIngredients
+        self.backgroundReductionIngredients = ingredients.backgroundReductionIngredients
+        self.calibrantSample = ingredients.calibrantSample
+        self.instrumentState = ingredients.instrumentState
+        self.focusGroup = ingredients.focusGroup
+        self.smoothDataIngredients = ingredients.smoothDataIngredients
+        pass
+
+    def PyExec(self):
+        ingredients: Ingredients = Ingredients.parse_raw(self.getProperty("Ingredients").value)
+        self.chopIngredients(ingredients)
+
+        inputWSName = str(self.getProperty("InputWorkspace").value)
+        backgroundWSName = str(self.getProperty("BackgroundWorkspace").value)
+
+        # run the algo
+        self.log().notice("Execution of CalibrationNormalizationAlgo START!")
+
+        rawVanadiumWSName = f"{inputWSName}_raw_vanadium_data"
+        self.mantidSnapper.RawVanadiumCorrectionAlgorithm(
+            "Correcting Vanadium Data...",
+            InputWorkspace=inputWSName,
+            BackgroundWorkspace=backgroundWSName,
+            CalibrationWorkspace=self.getProperty("CalibrationWorkspace").value,
+            Ingredients=self.reductionIngredients.json(),
+            CalibrantSample=self.calibrantSample.json(),
+            OutputWorkspace=rawVanadiumWSName,
+        )
+
+        groupingWSName = f"{inputWSName}_{self.focusGroup.name}_grouping_WS"
+        self.mantidSnapper.LoadGroupingDefinition(
+            f"Loading grouping file for focus group {self.focusGroup.name}...",
+            GroupingFilename=self.focusGroup.definition,
+            InstrumentDonor=rawVanadiumWSName,
+            OutputWorkspace=groupingWSName,
+        )
+
+        focusedWSName = f"{inputWSName}_{self.focusGroup.name}_focused_data"
+        self.mantidSnapper.DiffractionFocussing(
+            "Performing Diffraction Focusing ...",
+            InputWorkspace=rawVanadiumWSName,
+            GroupingWorkspace=groupingWSName,
+            OutputWorkspace=focusedWSName,
+            PreserveEvents=True,
+        )
+        self.mantidSnapper.executeQueue()
+
+        rebinRaggedFocussedWSName = f"data_rebinned_ragged_{inputWSName}_{self.focusGroup.name}"
+        if (
+            self.instrumentState.pixelGroupingInstrumentParameters is not None
+            and len(self.instrumentState.pixelGroupingInstrumentParameters) > 0
+        ):
+            pixelGroupingParam = self.instrumentState.pixelGroupingInstrumentParameters
+
+            dResolutionMin = [pgp.dResolution.minimum for pgp in pixelGroupingParam]
+            dResolutionMax = [pgp.dResolution.maximum for pgp in pixelGroupingParam]
+            dRelativeResolution = [
+                (pgp.dRelativeResolution / self.instrumentState.instrumentConfig.NBins) for pgp in pixelGroupingParam
+            ]
+
+            self.mantidSnapper.RebinRagged(
+                "Rebinning ragged bins...",
+                InputWorkspace=focusedWSName,
+                XMin=dResolutionMin,
+                XMax=dResolutionMax,
+                Delta=dRelativeResolution,
+                OutputWorkspace=rebinRaggedFocussedWSName,
+                PreserveEvents=True,
+            )
+
+        else:
+            raise Exception("Pixel grouping instrument parameters not defined!")
+
+        clonedWSName = f"{focusedWSName}_clone_focused_data"
+        self.mantidSnapper.CloneWorkspace(
+            "Cloning input workspace for lite data creation...",
+            InputWorkspace=rebinRaggedFocussedWSName,
+            OutputWorkspace=clonedWSName,
+        )
+
+        smoothedWSName = f"{inputWSName}_{self.focusGroup.name}_smoothed_ws"
+        self.mantidSnapper.SmoothDataExcludingPeaks(
+            "Fit and Smooth Peaks...",
+            InputWorkspace=clonedWSName,
+            Ingredients=self.smoothDataIngredients.json(),
+            OutputWorkspace=smoothedWSName,
+        )
+
+        self.mantidSnapper.WashDishes(
+            "Washing dishes...",
+            WorkspaceList=[
+                inputWSName,
+                backgroundWSName,
+                rawVanadiumWSName,
+                groupingWSName,
+                focusedWSName,
+                clonedWSName,
+            ],
+        )
+
+        self.mantidSnapper.executeQueue()
+
+        focusedWS = mtd[rebinRaggedFocussedWSName]
+        smoothedWS = mtd[smoothedWSName]
+
+        self.setProperty("FocusWorkspace", rebinRaggedFocussedWSName)
+        self.setProperty("SmoothWorkspace", smoothedWSName)
+
+        return focusedWS, smoothedWS
+
+
+# Register algorithm with Mantid
+AlgorithmFactory.subscribe(CalibrationNormalization)
---
+++ tests/cis_tests/normalization_calibration_script.py
@@ -0,0 +1,143 @@
+# import mantid algorithms, numpy and matplotlib
+from mantid.simpleapi import *
+import matplotlib.pyplot as plt
+import numpy as np
+
+from pydantic import parse_file_as
+import json
+
+from snapred.backend.recipe.algorithm.CalibrationNormalizationAlgo import CalibrationNormalization
+from snapred.backend.dao.ingredients.NormalizationCalibrationIngredients import NormalizationCalibrationIngredients
+from snapred.backend.dao.ingredients.SmoothDataExcludingPeaksIngredients import SmoothDataExcludingPeaksIngredients
+from snapred.backend.dao.ingredients.PixelGroupingIngredients import PixelGroupingIngredients
+from snapred.backend.service.CrystallographicInfoService import CrystallographicInfoService
+from snapred.backend.data.DataFactoryService import DataFactoryService
+from snapred.backend.dao.state import FocusGroup
+from snapred.backend.dao.state.CalibrantSample.CalibrantSamples import CalibrantSamples
+from snapred.backend.dao.calibration import CalibrationRecord, Calibration
+from snapred.backend.recipe.PixelGroupingParametersCalculationRecipe import PixelGroupingParametersCalculationRecipe
+from snapred.meta.Config import Config
+
+#User input ###############################################################################################
+runNumber = "58810"
+backgroundRunNumber = "58813"
+samplePath = "/SNS/SNAP/shared/Calibration_dynamic/CalibrantSamples/Silicon_NIST_640D_001.json"
+cifPath = "/SNS/SNAP/shared/Calibration/CalibrantSamples/Silicon_NIST_640d.cif"
+groupPath = "/SNS/SNAP/shared/Calibration/Powder/PixelGroupingDefinitions/SNAPFocGrp_Column.lite.xml"
+calibrationWorkspace = "/SNS/users/dzj/Desktop/58810_calibration_ws.nxs"
+smoothingParam = 0.50
+calibrationStatePath = "/SNS/SNAP/shared/Calibration/Powder/04bd2c53f6bf6754/57514/v_1/CalibrationParameters.json"
+###########################################################################################################
+
+DFS = DataFactoryService()
+
+#PixelGroupingCalulations #################################################################################
+
+def _generateFocusGroupAndInstrumentState(
+runNumber,
+definition,
+nBinsAcrossPeakWidth=Config["calibration.diffraction.nBinsAcrossPeakWidth"],
+calibration=None,):
+    if calibration is None:
+        calibration = DFS.getCalibrationState(runNumber)
+    instrumentState = calibration.instrumentState
+    pixelGroupingParams = _calculatePixelGroupingParameters(instrumentState, definition)["parameters"]
+    instrumentState.pixelGroupingInstrumentParameters = pixelGroupingParams
+    return (
+        FocusGroup(
+            FWHM=[pgp.twoTheta for pgp in pixelGroupingParams],
+            name=definition.split("/")[-1],
+            definition=definition,
+            nHst=len(pixelGroupingParams),
+            dMin=[pgp.dResolution.minimum for pgp in pixelGroupingParams],
+            dMax=[pgp.dResolution.maximum for pgp in pixelGroupingParams],
+            dBin=[pgp.dRelativeResolution / nBinsAcrossPeakWidth for pgp in pixelGroupingParams],
+        ),
+        instrumentState,
+)
+
+def _calculatePixelGroupingParameters(instrumentState, groupingFile: str):
+        groupingIngredients = PixelGroupingIngredients(
+            instrumentState=instrumentState,
+            instrumentDefinitionFile=Config["instrument.lite.definition.file"],
+            groupingFile=groupingFile,
+        )
+        try:
+            data = PixelGroupingParametersCalculationRecipe().executeRecipe(groupingIngredients)
+        except:
+            raise
+        return data
+
+#GetCalibrationRecord######################################################################################
+
+def getCalibrationState(calibrationStatePath):
+    calibrationState = parse_file_as(Calibration, calibrationStatePath)
+    return calibrationState
+
+###########################################################################################################
+
+#GetSampleFilePath#########################################################################################
+
+def getCalibrantSample(samplePath):
+    with open(samplePath, 'r') as file:
+        sampleJson = json.load(file)
+    del sampleJson["material"]["packingFraction"]
+    for atom in sampleJson["crystallography"]["atoms"]:
+        atom["symbol"] = atom.pop("atom_type")
+        atom["coordinates"] = atom.pop("atom_coordinates")
+        atom["siteOccupationFactor"] = atom.pop("site_occupation_factor")
+    sample = CalibrantSamples.parse_raw(json.dumps(sampleJson))
+    return sample
+###########################################################################################################
+
+reductionIngredients = DFS.getReductionIngredients(runNumber)
+backgroundReductionIngredients = DFS.getReductionIngredients(backgroundRunNumber)
+runConfig = DFS.getRunConfig(runNumber)
+calibration = getCalibrationState(calibrationStatePath)
+calibrantSample = getCalibrantSample(samplePath)
+crystalInfo = CrystallographicInfoService().ingest(cifPath)["crystalInfo"]
+
+focusGroup, instrumentState = _generateFocusGroupAndInstrumentState(
+runNumber,
+groupPath,
+)
+
+smoothDataIngredients = SmoothDataExcludingPeaksIngredients(
+    smoothingParameter=smoothingParam,
+    instrumentState=instrumentState,
+    crystalInfo=crystalInfo,
+)
+
+calibrationWorkspaceName = Load(calibrationWorkspace)
+
+
+ingredients = NormalizationCalibrationIngredients(
+reductionIngredients=reductionIngredients,
+backgroundReductionIngredients=backgroundReductionIngredients,
+calibrantSample=calibrantSample,
+focusGroup=focusGroup,
+instrumentState=instrumentState,
+smoothDataIngredients=smoothDataIngredients,
+)
+
+runIpts = reductionIngredients.runConfig.IPTS
+backIpts = backgroundReductionIngredients.runConfig.IPTS
+
+runVanadiumFilePath = runIpts + "shared/lite/SNAP_{}.lite.nxs.h5".format(
+    reductionIngredients.runConfig.runNumber
+)
+backgroundVanadiumFilePath = backIpts + "shared/lite/SNAP_{}.lite.nxs.h5".format(
+    backgroundReductionIngredients.runConfig.runNumber
+)
+
+input_WS = LoadEventNexus(runVanadiumFilePath)
+
+background_input_WS = LoadEventNexus(backgroundVanadiumFilePath)
+
+CNA = CalibrationNormalization()
+CNA.initialize()
+CNA.setProperty("InputWorkspace", input_WS)
+CNA.setProperty("BackgroundWorkspace", background_input_WS)
+CNA.setProperty("CalibrationWorkspace", calibrationWorkspaceName)
+CNA.setProperty("Ingredients", ingredients.json())
+CNA.execute()
