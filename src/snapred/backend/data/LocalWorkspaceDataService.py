d8f3be943af57ba5410938e6425ad4739a14cceb
Daniel Caballero <132241327+dlcaballero16@users.noreply.github.com>
2023-11-30 12:44:30 -0500

    FocusGroup refactoring changes (#162)
    * add a workspace name generator and integrate it with an algo
    
    * migrate localdataservice away from reductionparameters
    
    * First attempt at refactoring FocusGroups, more work will need to be done
    later
    
    * Moved PixelGroupingParameters inside of ReductionIngredients
    
    * Fixed issue with unit test failing
    
    * Wrapped output of WorkspaceNameGenerator to be more specific
    
    * Made suggestions changes from PR
    
    * Made changes based on PR comments
    
    * Addressing more PR comments
    
    * Fixed issue with unit test failing
    
    ---------
    
    Co-authored-by: Michael Walsh <walshmm@ornl.gov>

---
+++ src/snapred/backend/data/LocalWorkspaceDataService.py
@@ -0,0 +1,47 @@
+from mantid.api import AlgorithmManager, mtd
+
+from snapred.meta.decorators.Singleton import Singleton
+from snapred.meta.mantid.WorkspaceNameGenerator import WorkspaceName
+
+
+@Singleton
+class LocalWorkspaceDataService:
+    def __init__(self) -> None:
+        pass
+
+    def writeWorkspace(self, path: str, name: WorkspaceName):
+        """
+        Writes a Mantid Workspace to disk.
+        """
+        saveAlgo = AlgorithmManager.create("SaveNexus")
+        saveAlgo.setProperty("InputWorkspace", name)
+        saveAlgo.setProperty("Filename", path + name)
+        saveAlgo.execute()
+
+    def getWorkspaceForName(self, name: WorkspaceName):
+        """
+        Returns a workspace from Mantid if it exists.
+        Abstraction for the Service layer to interact with mantid data.
+        Usually we only deal in references as its quicker,
+        but sometimes its already in memory due to some previous step.
+        """
+        try:
+            return mtd[name]
+        except ValueError:
+            return None
+
+    def readWorkspaceCached(self, runId, name: WorkspaceName):
+        """
+        Use this in the case you anticipate loading fresh data from disk over and over.
+        """
+        pass
+
+    def deleteWorkspace(self, name: WorkspaceName):
+        """
+        Deletes a workspace from Mantid.
+        Mostly for cleanup at the Service Layer.
+        """
+        if self.getWorkspaceForName(name) is not None:
+            saveAlgo = AlgorithmManager.create("WashDishes")
+            saveAlgo.setProperty("Workspace", name)
+            saveAlgo.execute()
