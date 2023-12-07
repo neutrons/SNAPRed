8192fe9fd42b1cd5de59943d608e00bed0942f9a
Michael Walsh <walshmm@ornl.gov>
2023-12-07 08:50:42 -0500

    remove lying comment

--- src/snapred/backend/dao/state/DetectorState.py
+++ src/snapred/backend/dao/state/DetectorState.py
@@ -15,7 +15,6 @@ class DetectorState(BaseModel):
     arc: Tuple[float, float]
     wav: float
     freq: float
-    # Expected to only be 0 or 1, potentially should be a bool
     guideStat: Literal[GuideState.IN, GuideState.OUT]
     # two additional values that don't define state, but are useful
     lin: Tuple[float, float]
