8ab9efa16d422357493f36d5addc896ca09dc428
Michael Walsh <68125095+walshmm@users.noreply.github.com>
2023-12-07 09:31:55 -0500

    minimal change should auto switch between vals based on guide (#173)
    * minimal change should auto switch between vals based on guide
    
    * silly typo
    
    * its in instrumentstate not config
    
    * add an enum, according to Malcolm guidestat is actually 1-2, 1 = in
    
    * lets try this again
    
    * remove lying comment

--- src/snapred/backend/dao/state/DetectorState.py
+++ src/snapred/backend/dao/state/DetectorState.py
@@ -1,12 +1,20 @@
-from typing import Tuple
+from enum import IntEnum
+from typing import Literal, Tuple
 
 from pydantic import BaseModel
+
+# class syntax
+
+
+class GuideState(IntEnum):
+    IN = 1
+    OUT = 2
 
 
 class DetectorState(BaseModel):
     arc: Tuple[float, float]
     wav: float
     freq: float
-    guideStat: int
+    guideStat: Literal[GuideState.IN, GuideState.OUT]
     # two additional values that don't define state, but are useful
     lin: Tuple[float, float]
--- src/snapred/backend/dao/state/InstrumentState.py
+++ src/snapred/backend/dao/state/InstrumentState.py
@@ -6,7 +6,7 @@ from snapred.backend.dao.GSASParameters import GSASParameters
 from snapred.backend.dao.InstrumentConfig import InstrumentConfig
 from snapred.backend.dao.Limit import Limit
 from snapred.backend.dao.ParticleBounds import ParticleBounds
-from snapred.backend.dao.state.DetectorState import DetectorState
+from snapred.backend.dao.state.DetectorState import DetectorState, GuideState
 from snapred.backend.dao.state.PixelGroupingParameters import PixelGroupingParameters
 
 
@@ -19,3 +19,11 @@ class InstrumentState(BaseModel):
     defaultGroupingSliceValue: float
     fwhmMultiplierLimit: Limit[float]
     peakTailCoefficient: float
+
+    @property
+    def delTh(self) -> float:
+        return (
+            self.instrumentConfig.delThWithGuide
+            if self.detectorState.guideStat == GuideState.IN
+            else self.instrumentConfig.delThNoGuide
+        )
