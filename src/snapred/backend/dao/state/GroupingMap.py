b68fb0cd8d8d1538a9afbf366baf18e4b303fa0f
Daniel Caballero <dlcaballero16@gmail.com>
2023-12-22 11:01:57 -0500

    Added unit tests for GroupingMap, made save and load functions

--- src/snapred/backend/dao/state/GroupingMap.py
+++ src/snapred/backend/dao/state/GroupingMap.py
@@ -1,34 +1,90 @@
+from pathlib import Path
 from typing import Dict, List
 
 from pydantic import BaseModel
 
+# import json
 from snapred.backend.dao.state.FocusGroup import FocusGroup
+from snapred.backend.log.logger import snapredLogger
+from snapred.meta.Config import Config, Resource
+from snapred.meta.redantic import write_model
+
+logger = snapredLogger.getLogger(__name__)
 
 
 class GroupingMap(BaseModel):
-    focusGroupMapping: Dict[str, Dict[str, FocusGroup]] = {}
-    SHA: str = None
+    availableGroups: Dict[str, Dict[str, Path]] = {}
+    stateID: str = None
 
     @property
-    def lite(self) -> Dict[str, FocusGroup]:
-        return self.focusGroupMapping["lite"]
+    def lite(self) -> Dict[str, Path]:
+        return self.availableGroups["lite"]
 
     @property
-    def native(self) -> Dict[str, FocusGroup]:
-        return self.focusGroupMapping["native"]
+    def native(self) -> Dict[str, Path]:
+        return self.availableGroups["native"]
 
     def __init__(
         self,
         liteFocusGroups: List[FocusGroup] = None,
         nativeFocusGroups: List[FocusGroup] = None,
-        focusGroupMapping={},
-        SHA: str = None,
+        availableGroups={},
+        stateID: str = None,
     ):
-        focusGroupMapping = {
-            "native": {nfg["name"]: nfg for nfg in nativeFocusGroups},
-            "lite": {lfg["name"]: lfg for lfg in liteFocusGroups},
+        nativeMap = {}
+        liteMap = {}
+        if nativeFocusGroups is None:
+            logger.warning("No FocusGroups for native mode given")
+        else:
+            for nfg in nativeFocusGroups:
+                if not self.validateGroupingFile(nfg["definition"]):
+                    continue
+                else:
+                    nativeMap[nfg["name"]] = nfg["definition"]
+        if liteFocusGroups is None:
+            logger.warning("No FocusGroups for lite mode given")
+        else:
+            for lfg in liteFocusGroups:
+                if not self.validateGroupingFile(lfg["definition"]):
+                    continue
+                else:
+                    liteMap[lfg["name"]] = lfg["definition"]
+        availableGroups = {
+            "native": nativeMap,
+            "lite": liteMap,
         }
         return super().__init__(
-            focusGroupMapping=focusGroupMapping,
-            SHA=SHA,
+            availableGroups=availableGroups,
+            stateID=stateID,
         )
+
+    def validateGroupingFile(self, fp: str = None):
+        supportedExtensions = ["H5", "HD5", "HDF", "NXS", "NXS5", "XML"]
+        if not Path(fp).exists():
+            logger.warning("File NOT FOUND")
+            return False
+        if not Path().is_file():
+            logger.warning("not valid file")
+            return False
+        if not fp.endswith(supportedExtensions):
+            logger.warning("file format is wrong")
+            return False
+        return True
+
+    @classmethod
+    def load(cls, stateID: str):
+        if stateID == "default":
+            # DefaultMap location and overwrite calibration.grouping.home
+            # write to file here
+            return GroupingMap.parse_raw(Resource.read(Config["calibration.grouping.home"]))
+        else:
+            # load map from location
+            return GroupingMap.parse_raw(Resource.read(stateID + "/inputs/SampleGroupingFile.json"))
+
+    @classmethod
+    def save(cls, stateID: str):
+        if stateID != "default":
+            print("about to write file")
+            write_model(cls, stateID + Config["instrument.state.home"])
+        else:
+            logger.warning("cannot overwrite default mapping, change stateID")
--- tests/unit/backend/dao/test_GroupingMap.py
+++ tests/unit/backend/dao/test_GroupingMap.py
@@ -1,16 +1,72 @@
-import unittest
+from unittest.mock import patch
 
+import pytest
 from snapred.backend.dao.state.GroupingMap import GroupingMap
 from snapred.meta.Config import Resource
 
 
-class TestDiffractionCalibtationRecipe(unittest.TestCase):
-    def setUp(self):
-        groupingMap = GroupingMap.parse_raw(Resource.read("/inputs/SampleGroupingFile.json"))
-        # this prints out the FocusGroup in the lite map containing the Name all
-        print(groupingMap.lite["All"])
-        print(groupingMap.SHA)
+class TestDiffractionCalibtationRecipe:
+    def setup_method(self):
+        self.groupingMap = GroupingMap.parse_raw(Resource.read("/inputs/SampleGroupingFile.json"))
 
-    def test_something(self):
-        # this makes the test fail so we can see the output
+    # Tests of basic serialization: read / write to and from a JSON file on disk;
+    # Validator tests: grouping-file doesn't exist;  correct behavior is to log,
+    #  and continue to load (but _omit_ these groupings from the loaded groupingMap);
+    @patch("snapred.backend.log.logger")
+    def test_file_does_not_exist(self, mockLogger):
+        GroupingMap.parse_obj(
+            {
+                "stateID": "1",
+                "liteFocusGroups": [
+                    {"name": "Column", "definition": "invalid path"},
+                    {"name": "All", "definition": "valid path"},
+                ],
+                "nativeFocusGroups": [
+                    {"name": "Column", "definition": "valid path"},
+                    {"name": "All", "definition": "invalid path"},
+                ],
+            }
+        )
+        # figure out assert call to logger
+        mockLogger.assert_called_once()
+
+    # Validator tests: grouping-file is not actually a grouping file; correct behavior is to log and continue to load;
+    def test_file_wrong_format(self):
+        GroupingMap.parse_obj(
+            {
+                "stateID": "2",
+                "liteFocusGroups": [{"name": "Column", "definition": "wrong file format"}],
+                "nativeFocusGroups": [{"name": "Column", "definition": "wrong file format"}],
+            }
+        )
+        # figure out assert call to logger
         assert False
+
+    # Validator tests: no grouping files are listed in the JSON;
+    def test_empty_list(self):
+        mockGroupingMap = GroupingMap.parse_obj({"stateID": "3", "liteFocusGroups": [], "nativeFocusGroups": []})
+        assert len(mockGroupingMap.lite) == 0
+
+    # Tests of loading from the default location, with the <state ID> specified as '_default_';
+    # for the moment, use '~/_test_tmp/calibration.grouping.home' as the location, until the SNS-filesystem mock is ready;
+    def test_load_default(self):
+        groupingMap = GroupingMap.load("default")
+        assert groupingMap.stateID == "default"
+
+    # Tests of setting the state ID (from the actual state) and writing to the 'instrument.state.root' location;
+    #  for the moment, use '~/_test_tmp/instrument.state.home/<state SHA>'  "   ";
+    def test_save_state(self):
+        mockGroupingMap = GroupingMap.parse_obj(
+            {
+                "stateID": "4",
+                "liteFocusGroups": [{"name": "Column", "definition": "path"}, {"name": "All", "definition": "path"}],
+                "nativeFocusGroups": [{"name": "Column", "definition": "path"}, {"name": "All", "definition": "path"}],
+            }
+        )
+        mockGroupingMap.save("4")
+        loadedGroupingMap = GroupingMap.load("4")
+        assert loadedGroupingMap.stateID == "4"
+
+
+# Any logged messages should be helpful for any normal end user, and also for a CIS dealing with this system.
+# For example, if there is a JSON-format error, a log message should indicate where the example _default_ version of the file is located, for comparison.
