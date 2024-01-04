7f2a196391964dc13078a6ee89d6b20aa20f1cdf
Reece Boston <52183986+rboston628@users.noreply.github.com>
2024-01-04 14:20:02 -0500

    Limit iterations of PixelDiffractionCalibration (#189)
    * task 1: hardcaps = done
    
    * move magic numbers to application.yml
    
    * change default convergence threshold, also implement max offset as 10 in config
    
    * update old tests and check monotonic
    
    * add guard to pixel cal to not update difc if offsets not converging
    
    * pixel cal median or mean?
    
    * clarify monotonic test

--- src/snapred/backend/dao/request/DiffractionCalibrationRequest.py
+++ src/snapred/backend/dao/request/DiffractionCalibrationRequest.py
@@ -20,3 +20,4 @@ class DiffractionCalibrationRequest(BaseModel):
     convergenceThreshold: Optional[float] = Config["calibration.diffraction.convergenceThreshold"]
     peakIntensityThreshold: Optional[float] = Config["calibration.diffraction.peakIntensityThreshold"]
     nBinsAcrossPeakWidth: int = Config["calibration.diffraction.nBinsAcrossPeakWidth"]
+    maximumOffset: float = Config["calibration.diffraction.maximumOffset"]
