75b16d9dcb5e3e9258ac97b714780402eea7c405
Daniel Caballero <dlcaballero16@gmail.com>
2024-01-10 10:24:40 -0500

    Added more fields and validators to GroupingMap, updated GroupingMap json files to contain what is expected, added unit tests for GroupingMap

--- src/snapred/backend/dao/state/GroupingMap.py
+++ src/snapred/backend/dao/state/GroupingMap.py
@@ -1,17 +1,12 @@
-from typing import List, Optional, Any, ClassVar
+from typing import Any, ClassVar, Dict
+from pathlib import Path
 
 from pydantic import BaseModel, Field, validator, root_validator
-from pydantic.error_wrappers import ValidationError
 
-from snapred.backend.error.StateValidationException import StateValidationException
 from snapred.backend.dao.ObjectSHA import ObjectSHA
-from snapred.backend.dao.StateId import StateId
-from snapred.backend.dao.GSASParameters import GSASParameters
-from snapred.backend.dao.InstrumentConfig import InstrumentConfig
-from snapred.backend.dao.Limit import Limit
-from snapred.backend.dao.ParticleBounds import ParticleBounds
-from snapred.backend.dao.state.DetectorState import DetectorState, GuideState
-from snapred.backend.dao.state.PixelGroup import PixelGroup
+from snapred.backend.log.logger import snapredLogger
+
+logger = snapredLogger.getLogger(__name__)
 
 
 class GroupingMap(BaseModel):
@@ -25,10 +20,23 @@ class GroupingMap(BaseModel):
     stateId: ObjectSHA
 
     _isDirty: bool = Field(default=False, kw_only=True)
+    # availableGroups: Dict[str, Dict[str, Path]] = {}
+    nativeGroups: Dict[str, Path] = Field(default=None, exclude=True)
+    liteGroups: Dict[str, Path] = Field(default=None, exclude=True)
+
+    @property
+    def lite(self) -> Dict[str, Path]:
+        return self.liteGroups
+
+    @property
+    def native(self) -> Dict[str, Path]:
+        return self.nativeGroups
     
+    @property
     def isDefault(self):
         return self.id == GroupingMap.defaultId
 
+    @property
     def isDirty(self) -> bool:
         return self._isDirty
     
@@ -36,10 +44,10 @@ class GroupingMap(BaseModel):
         self._isDirty = flag
 
     def cloneWithNewStateId(self, stateId: ObjectSHA) -> 'GroupingMap':
-        return type(self)(\
+        return type(self)(
             stateId = stateId,
-            _isDirty = True,\
-            # other fields go here...
+            _isDirty = True,
+            availableGroups = self.availableGroups
         )
             
     @validator('stateId', pre=True, allow_reuse=True)
@@ -47,4 +55,33 @@ class GroupingMap(BaseModel):
         # ObjectSHA stored in JSON as _only_ a single hex string, for the hex digest itself
         if isinstance(v, str):
             return ObjectSHA(hex=v, decodedKey=None)
+        return v
+    
+    @root_validator(pre=False, allow_reuse=True)
+    def validate_GroupingMapFile(cls, v):
+        nativeMap = {}
+        liteMap = {}
+        if v.get("nativeFocusGroups"):
+            nativeMap = {nfg["name"]: nfg["definition"] for nfg in v["nativeFocusGroups"]}
+        if v.get("liteFocusGroups"):
+            liteMap = {lfg["name"]: lfg["definition"] for lfg in v["liteFocusGroups"]}
+        groups = {"native": nativeMap, "lite": liteMap}
+        supportedExtensions = ("H5", "HD5", "HDF", "NXS", "NXS5", "XML")
+        for mode in groups.copy():
+            for group in groups[mode].copy():
+                fp = groups[mode][group]
+                if not fp.exists():
+                    logger.warning("File:" + str(fp) + " not found")
+                    del groups[mode][group]
+                    continue
+                if not fp.is_file():
+                    logger.warning("File supplied:" + str(fp) + " is not valid")
+                    del groups[mode][group]
+                    continue
+                if not str(fp).endswith(supportedExtensions):
+                    logger.warning("File format for:" + str(fp) + " is wrong")
+                    del groups[mode][group]
+                    continue
+            if mode == {}:
+                logger.warning("No FocusGroups for " + mode + " given")
         return v
