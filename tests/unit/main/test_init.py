76528165fa3f5dcfe64979fedf98038ff77615c5
Daniel Caballero <dlcaballero16@gmail.com>
2024-01-16 12:22:47 -0500

    Removed skip from test

--- tests/unit/main/test_init.py
+++ tests/unit/main/test_init.py
@@ -1,12 +1,9 @@
-import pytest
-
 # Pull data from the dummy package.
 from .init import aII, localz
 
 
 # Test the pullModuleMembers function.
 # Ensures that the right modules and contents are pulled.
-@pytest.mark.skip(reason="Temporary regression?")
 def test_pullCorrectVals():
     assert "dummy" in aII
     assert "restricted_dummy" in aII
