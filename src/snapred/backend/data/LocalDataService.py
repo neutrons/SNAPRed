2269f6489eb6c1bf64f27dee67661515853fa64a
Robert Bolotovsky <bolotovskyr@ornl.gov>
2024-01-16 14:02:55 -0500

    Add DataFactoryService and LocalDataService unit tests.

--- src/snapred/backend/data/LocalDataService.py
+++ src/snapred/backend/data/LocalDataService.py
@@ -444,6 +444,7 @@ class LocalDataService:
         else:
             recordPath: str = self.getCalibrationRecordPath(runId, "*")
             recordFile = self._getLatestFile(recordPath)
+        print(f"*****RECORD PATH: {recordPath}")
         record: CalibrationRecord = None
         if recordFile:
             logger.info(f"reading CalibrationRecord from {recordFile}")
