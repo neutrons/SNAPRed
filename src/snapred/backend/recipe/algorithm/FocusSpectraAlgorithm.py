83134855593deef50f2442679e22ce93583a084b
Michael Walsh <walshmm@ornl.gov>
2024-01-17 10:27:32 -0500

    use correct kwarg name

--- src/snapred/backend/recipe/algorithm/FocusSpectraAlgorithm.py
+++ src/snapred/backend/recipe/algorithm/FocusSpectraAlgorithm.py
@@ -33,7 +33,9 @@ class FocusSpectraAlgorithm(PythonAlgorithm):
             MatrixWorkspaceProperty("OutputWorkspace", "", Direction.Output, PropertyMode.Optional),
             doc="The diffraction-focused data",
         )
-        self.declareProperty("Ingredients", defaultValue="", direction=Direction.Input, optional=PropertyMode.Mandatory)
+        self.declareProperty(
+            "Ingredients", defaultValue="", direction=Direction.Input, propertyMode=PropertyMode.Mandatory
+        )
         self.setRethrows(True)
         self.mantidSnapper = MantidSnapper(self, __name__)
 
