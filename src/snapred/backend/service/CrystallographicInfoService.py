9110c34539683330cf2a42724c5cc7c1450789d1
Boston S R <4rx@analysis-node18.sns.gov>
2024-01-19 11:45:34 -0500

    add defaults to service to prevent error with no dMin

--- src/snapred/backend/service/CrystallographicInfoService.py
+++ src/snapred/backend/service/CrystallographicInfoService.py
@@ -2,13 +2,18 @@ from typing import Any, Dict
 
 from snapred.backend.recipe.CrystallographicInfoRecipe import CrystallographicInfoRecipe
 from snapred.backend.service.Service import Service
+from snapred.meta.Config import Config
 from snapred.meta.decorators.FromString import FromString
 from snapred.meta.decorators.Singleton import Singleton
 
 
 @Singleton
 class CrystallographicInfoService(Service):
+    D_MIN = Config["constants.CrystallographicInfo.dMin"]
+    D_MAX = Config["constants.CrystallographicInfo.dMax"]
+
     # register the service in ServiceFactory please!
+    # NEVER!
     def __init__(self):
         super().__init__()
         self.registerPath("", self.ingest)
@@ -19,11 +24,11 @@ class CrystallographicInfoService(Service):
         return "ingestion"
 
     @FromString
-    def ingest(self, cifPath: str, dMin: float) -> Dict[Any, Any]:
+    def ingest(self, cifPath: str, dMin: float = D_MIN, dMax: float = D_MAX) -> Dict[Any, Any]:
         data: Dict[Any, Any] = {}
         # TODO: collect runs by state then by calibration of state, execute sets of runs by calibration of thier state
         try:
-            data = CrystallographicInfoRecipe().executeRecipe(cifPath, dMin=dMin)
+            data = CrystallographicInfoRecipe().executeRecipe(cifPath, dMin=dMin, dMax=dMax)
         except:
             raise
         return data
