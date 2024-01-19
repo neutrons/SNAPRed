a8ee745c93283e490f2349e18e364fd0442518a4
Boston S R <4rx@analysis-node18.sns.gov>
2024-01-19 11:56:14 -0500

    unit tests schmunit tests

--- tests/unit/backend/service/test_CrystallographicInfoService.py
+++ tests/unit/backend/service/test_CrystallographicInfoService.py
@@ -1,6 +1,7 @@
 from unittest.mock import patch
 
 from snapred.backend.service.CrystallographicInfoService import CrystallographicInfoService
+from snapred.meta.Config import Config
 
 
 @patch("snapred.backend.service.CrystallographicInfoService.CrystallographicInfoRecipe")
@@ -10,5 +11,5 @@ def test_CrystallographicInfoService(mockRecipe):
     assert service.name() == "ingestion"
 
     result = service.ingest("cifPath", 1.0)
-    mockRecipe.executeRecipe.assert_called_once_with("cifPath", dMin=1.0)
+    mockRecipe.executeRecipe.assert_called_once_with("cifPath", dMin=1.0, dMax=Config["constants.CrystallographicInfo.dMax"])
     assert result == mockRecipe.executeRecipe.return_value
