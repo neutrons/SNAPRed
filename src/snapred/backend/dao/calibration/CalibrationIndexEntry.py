a5030b203f2c18c2343847737857f7c15448ac1a
Michael Walsh <walshmm@ornl.gov>
2024-02-26 16:39:38 -0500

    update validator to account for new symbols

--- src/snapred/backend/dao/calibration/CalibrationIndexEntry.py
+++ src/snapred/backend/dao/calibration/CalibrationIndexEntry.py
@@ -13,19 +13,31 @@ class CalibrationIndexEntry(BaseModel):
     author: str
     timestamp: Optional[int]
 
+    def parseAppliesTo(appliesTo: str):
+        symbols = [">=", "<=", "<", ">"]
+        # find first
+        symbol = next((s for s in symbols if s in appliesTo), "")
+        # parse runnumber
+        runNumber = appliesTo if symbol == "" else appliesTo.split(symbol)[-1]
+        return symbol, runNumber
+
     @validator("appliesTo", allow_reuse=True)
     def appliesToFormatChecker(cls, v):
         """
         This validator ensures that if appliesTo is present,
-        it is in the format of 'runNumber', '>runNumber', or '<runNumber'.
+        it is in the format of 'runNumber', or '{symbol}runNumber' where symbol is one of '>', '<', '>=', '<='.
         """
         testValue = v
         if testValue is not None:
-            if testValue.startswith(">") or testValue.startswith("<"):
+            symbol, _ = cls.parseAppliesTo(v)
+            if symbol != "":
                 testValue = testValue[1:]
-                try:
-                    int(testValue)
-                except ValueError:
-                    raise ValueError("appliesTo must be in the format of 'runNumber', '>runNumber', or '<runNumber'.")
+            try:
+                int(testValue)
+            except ValueError:
+                raise ValueError(
+                    "appliesTo must be in the format of 'runNumber',"
+                    "or '\{symbol\}runNumber' where symbol is one of '>', '<', '>=', '<='.."
+                )
 
         return v
