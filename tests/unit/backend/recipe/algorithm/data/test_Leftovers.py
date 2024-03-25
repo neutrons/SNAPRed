b0baedd650d8449a2657113a8652a19ecd79ec05
Michael Walsh <walshmm@ornl.gov>
2024-03-25 09:58:51 -0400

    forgot to adjust rebinragged params for less spectras

--- tests/unit/backend/recipe/algorithm/data/test_Leftovers.py
+++ tests/unit/backend/recipe/algorithm/data/test_Leftovers.py
@@ -36,11 +36,12 @@ def test_saveLoad():
     )
     LoadInstrument(Workspace="raw", RewriteSpectraMap=False, InstrumentName="SNAP")
 
-    xMin = [0.05] * 20
+    numFillerSpectra = 10 - 6
+    xMin = [0.05] * numFillerSpectra
     xMin.extend([0.05, 0.06, 0.1, 0.07, 0.04, 0.04])
-    xMax = [0.36] * 20
+    xMax = [0.36] * numFillerSpectra
     xMax.extend([0.36, 0.41, 0.64, 0.48, 0.48, 0.48])
-    delta = [-0.000401475] * 20
+    delta = [-0.000401475] * numFillerSpectra
     delta.extend([-0.000401475, -0.000277182, -0.000323453, -0.000430986, -0.000430986, -0.000430986])
 
     RebinRagged(InputWorkspace="raw", XMin=xMin, XMax=xMax, Delta=delta, PreserveEvents=False, OutputWorkspace="raw")
