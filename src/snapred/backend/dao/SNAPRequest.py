b48e4ff3a684b7c98ca493bca0bfc830abd8c529
Darsh <ddarsh5544@gmail.com>
2024-04-04 08:34:30 -0400

    Missed docs

--- src/snapred/backend/dao/SNAPRequest.py
+++ src/snapred/backend/dao/SNAPRequest.py
@@ -4,7 +4,13 @@ from pydantic import BaseModel
 
 
 class SNAPRequest(BaseModel):
-    """ """
+    """
+
+    `SNAPRequest` represents the structure of a request within the SNAPRed,
+    containing a path indicating the target of the request and an optional
+    payload that carries the request's data.
+
+    """
 
     path: str
     payload: Optional[str] = None
