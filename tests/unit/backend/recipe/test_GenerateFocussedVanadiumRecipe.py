823c828e5005bfba7d94426998473ac0906e3f59
Darsh Dinger <106342815+darshdinger@users.noreply.github.com>
2024-04-22 07:57:21 -0500

    EWM4794 - Generate Focussed Vanadium Recipe (#304)
    * Recipe and pertaining ingredients
    
    * Failing test, having issues with unit test
    
    * Tests finally corrected!
    
    * update to v4, actually pass a token (#306)
    
    * updates to recipe
    
    * updates to recipe and test
    
    * comment updates
    
    ---------
    
    Co-authored-by: Michael Walsh <68125095+walshmm@users.noreply.github.com>

---
+++ tests/unit/backend/recipe/test_GenerateFocussedVanadiumRecipe.py
@@ -0,0 +1,75 @@
+import unittest
+from unittest import mock
+
+import pytest
+from mantid.simpleapi import (
+    CreateWorkspace,
+    LoadNexusProcessed,
+    mtd,
+)
+from snapred.backend.dao.GroupPeakList import GroupPeakList
+from snapred.backend.dao.ingredients import GenerateFocussedVanadiumIngredients as Ingredients
+from snapred.backend.recipe.algorithm.SmoothDataExcludingPeaksAlgo import SmoothDataExcludingPeaksAlgo
+from snapred.backend.recipe.GenerateFocussedVanadiumRecipe import GenerateFocussedVanadiumRecipe as Recipe
+from snapred.meta.Config import Resource
+from util.helpers import deleteWorkspaceNoThrow
+from util.SculleryBoy import SculleryBoy
+
+ThisRecipe: str = "snapred.backend.recipe.GenerateFocussedVanadiumRecipe"
+SmoothAlgo: str = ThisRecipe + ".SmoothDataExcludingPeaksAlgo"
+
+
+class TestGenerateFocussedVanadiumRecipe(unittest.TestCase):
+    def setUp(self):
+        testWorkspaceFile = "inputs/strip_peaks/DSP_58882_cal_CC_Column_spectra.nxs"
+
+        self.fakeInputWorkspace = "_test_input_workspace"
+        self.fakeOutputWorkspace = "_test_smoothed_output"
+
+        LoadNexusProcessed(
+            Filename=Resource.getPath(testWorkspaceFile),
+            outputWorkspace=self.fakeInputWorkspace,
+        )
+        peaks = SculleryBoy().prepDetectorPeaks({})
+
+        self.fakeIngredients = Ingredients(
+            smoothingParameter=0.1,
+            detectorPeaks=peaks,
+        )
+        self.groceryList = {
+            "inputWorkspace": self.fakeInputWorkspace,
+            "outputWorkspace": self.fakeOutputWorkspace,
+        }
+        self.errorGroceryList = {
+            "inputWorkspace": None,
+            "outputWorkspace": self.fakeOutputWorkspace,
+        }
+        self.recipe = Recipe()
+
+    def tearDown(self) -> None:
+        workspaces = mtd.getObjectNames()
+        for ws in workspaces:
+            deleteWorkspaceNoThrow(ws)
+        return super().tearDown()
+
+    @mock.patch(SmoothAlgo)
+    def test_execute_successful(self, mockSmoothAlgo):
+        mock_instance = mockSmoothAlgo.return_value
+        mock_instance.execute.return_value = None
+        mock_instance.getPropertyValue.return_value = self.fakeOutputWorkspace
+
+        expected_output = {"outputWorkspace": self.fakeOutputWorkspace, "result": True}
+
+        output = self.recipe.executeRecipe(self.fakeIngredients, self.groceryList)
+
+        self.assertEqual(output, expected_output)  # noqa: PT009
+
+    @mock.patch(SmoothAlgo)
+    def test_execute_unsuccessful(self, mockSmoothAlgo):
+        mock_instance = mockSmoothAlgo.return_value
+        mock_instance.execute.return_value = None
+        mock_instance.getPropertyValue.return_value = None
+
+        with pytest.raises(NotImplementedError) as e:
+            self.recipe.executeRecipe(self.fakeIngredients, self.errorGroceryList)
+        assert str(e.value) == "Fake Vanadium not implemented yet."  # noqa: PT009
