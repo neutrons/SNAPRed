19f70964fabe9f7b5da1e3f6692bc5859b9f44ad
Darsh Dinger <106342815+darshdinger@users.noreply.github.com>
2024-04-30 10:01:17 -0500

    User Docs button implementation (#312)
    * User Docs button implementation
    
    * Updates to fix docs loading error
    
    * Missed a breakpoint()
    
    * test: add comment
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * commit from other source
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update to environment .yml. (temporary)
    
    * Reece's requested updates
    
    ---------
    
    Co-authored-by: Kort Travis <traviska@ornl.gov>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: rboston628 <bostonsr@ornl.gov>
    Co-authored-by: Reece Boston <52183986+rboston628@users.noreply.github.com>

---
+++ src/snapred/ui/widget/UserDocsButton.py
@@ -0,0 +1,40 @@
+from PyQt5.QtCore import QUrl
+from PyQt5.QtWebEngineWidgets import QWebEngineView
+from PyQt5.QtWidgets import QHBoxLayout, QPushButton, QWidget
+
+from snapred.meta.Config import Config
+
+# ref: unit-test failures
+
+# test
+
+
+class UserDocsButton(QWidget):
+    def __init__(self, parent=None):
+        super(UserDocsButton, self).__init__(parent)
+        self.setStyleSheet("background-color: #F5E9E2;")
+        self.initUI()
+
+    def initUI(self):
+        # Layout setup
+        layout = QHBoxLayout(self)
+        self.setLayout(layout)
+
+        # Button to launch the web view
+        self.button = QPushButton("User Documentation", self)
+        self.button.setStyleSheet("background-color: #F5E9E2;")
+        self.button.clicked.connect(self.launchWebView)
+        layout.addWidget(self.button)
+
+    def launchWebView(self):
+        # Create and configure the web view
+        self.webView = QWebEngineView()
+        self.webView.setWindowTitle("Documentation")
+        self.webView.resize(800, 600)
+
+        # Point to the specific file on the filesystem
+        url = QUrl.fromLocalFile(str(Config["docs.user.path"]))
+        self.webView.setUrl(url)
+
+        # Show the web view
+        self.webView.show()
