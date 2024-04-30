1efae19a3a74d8a2c2ea248381b15d2bef5fae39
Darsh <ddarsh5544@gmail.com>
2024-04-30 08:26:13 -0400

    Created ReductionRecord.py

---
+++ src/snapred/backend/dao/reduction/ReductionRecord.py
@@ -0,0 +1,46 @@
+from typing import List
+
+from pydantic import BaseModel
+
+from snapred.backend.dao.normalization.NormalizationRecord import NormalizationRecord
+from snapred.backend.dao.calibration.CalibrationRecord import CalibrationRecord
+from snapred.backend.dao.state.PixelGroupingParameters import PixelGroupingParameters
+
+class ReductionRecord(BaseModel):
+    """ 
+    
+    This class plays a fundamental role in the data reduction process, central to refining and 
+    transforming raw experimental data into a format suitable for scientific analysis. It acts as 
+    a detailed repository or record for capturing the entirety of the reduction steps, encompassing
+    calibration, normalization, and pixel grouping details.
+
+    """
+
+    calibrationRecord: CalibrationRecord
+    normalizationRecord: NormalizationRecord
+    pixelGroupingParameters: List[PixelGroupingParameters]
+
+    # placeholders for later phase 3 additions (bound to change)
+
+    """
+
+    *Details of the reduction*:
+
+    containerType:
+    backgroundType:
+    backgroundData:
+    containerMasks:
+    geometricProperties:
+    crystallographicProperties:
+    materialProperties:
+    attenuationType:
+    attenuationData:
+    attenuationsParameters:
+
+
+    *Details of post reduction processing*:
+
+    dSAppliedData: (dS == down sampling)
+    dSAppliedParameters: (rebinning parameters)
+
+    """
