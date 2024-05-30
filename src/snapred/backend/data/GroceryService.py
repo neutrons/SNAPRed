b04da33b217de501be221505a2cd9894d2b39d0b
Michael Walsh <walshmm@ornl.gov>
2024-05-30 13:53:51 -0400

    unrevert groceryservice such that diffcal_table is looked up via targetRunnumber -> calibrationRecord -> table in calib version named with calibRecord.runnumber

--- src/snapred/backend/data/GroceryService.py
+++ src/snapred/backend/data/GroceryService.py
@@ -879,9 +879,11 @@ class GroceryService:
                         item.version = self.dataService._getVersionFromCalibrationIndex(
                             item.runNumber, item.useLiteMode
                         )
+                        record = self.dataService.readCalibrationRecord(item.runNumber, item.useLiteMode, item.version)
                     tableWorkspaceName = self._createDiffcalTableWorkspaceName(
-                        item.runNumber, item.useLiteMode, item.version
+                        record.runNumber, item.useLiteMode, item.version
                     )
+                    item.runNumber = record.runNumber
                     if item.isOutput:
                         res = {"result": True, "workspace": tableWorkspaceName}
                     else:
