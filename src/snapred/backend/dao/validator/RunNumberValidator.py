24a22fd4853368aa6998b6bff3082b33acc23c73
Daniel Caballero <dlcaballero16@gmail.com>
2024-06-17 16:30:48 -0400

    Removed commented code

--- src/snapred/backend/dao/validator/RunNumberValidator.py
+++ src/snapred/backend/dao/validator/RunNumberValidator.py
@@ -9,18 +9,15 @@ class RunNumberValidator(BaseModel):
     @classmethod
     def validateRunNumber(cls, runNumber: str):
         if not runNumber.isdigit():
-            # raise RecoverableException("Run number must be a positive integer")
             logger.warning("Run number must be a positive integer")
             return False
         number = int(runNumber)
         minRunNumber = Config["instrument.startingRunNumber"]
         maxRunNumber = Config["instrument.maximumRunNumber"]
         if number < minRunNumber:
-            # raise RecoverableException("Run number is below minimum value of " + str(minRunNumber))
             logger.warning("Run number is below minimum value of " + str(minRunNumber))
             return False
         if number > maxRunNumber:
-            # raise RecoverableException("Run number exceeds max value of " + str(maxRunNumber))
             logger.warning("Run number exceeds max value of " + str(maxRunNumber))
             return False
         return True
