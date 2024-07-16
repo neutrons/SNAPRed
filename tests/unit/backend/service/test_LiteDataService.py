e1212777bc807b2ef2e0bb05afb8e78125d80cfe
Darsh <ddarsh5544@gmail.com>
2024-07-16 15:21:54 -0400

    Fixed test_liteDataService.py

--- tests/unit/backend/service/test_LiteDataService.py
+++ tests/unit/backend/service/test_LiteDataService.py
@@ -6,21 +6,14 @@ from unittest.mock import Mock, patch
 
 import pytest
 from mantid.simpleapi import CloneWorkspace, CreateSingleValuedWorkspace
-from snapred.backend.dao.calibration import Calibration
-from snapred.backend.dao.ingredients.PixelGroupingIngredients import PixelGroupingIngredients
-from snapred.backend.dao.state.InstrumentState import InstrumentState
 from snapred.backend.service.LiteDataService import LiteDataService
 from snapred.meta.Config import Resource
 
 
 class TestLiteDataService(unittest.TestCase):
     @patch("snapred.backend.service.LiteDataService.Recipe.executeRecipe")
-    @patch("snapred.backend.service.LiteDataService.DataFactoryService.getCalibrationState")
-    @patch("snapred.backend.service.LiteDataService.DataExportService.exportWorkspace")
     def test_reduceLiteData_calls_executeRecipe_with_correct_arguments(
         self,
-        mock_exportWorkspace,  # noqa: ARG002
-        mock_getCalibrationState,
         executeRecipe,
     ):
         executeRecipe.return_value = {}
@@ -29,21 +22,16 @@ class TestLiteDataService(unittest.TestCase):
             OutputWorkspace=kwargs["OutputWorkspace"],
         )
 
-        # Mock the calibration state and ingredients
-        mock_calibration = Mock(spec=Calibration)
-        mock_instrument_state = Mock(spec=InstrumentState)
-        mock_ingredients = Mock(spec=PixelGroupingIngredients)
-        mock_ingredients.model_dump_json.return_value = '{"instrumentConfig": {}}'  # Add appropriate JSON content
-        mock_instrument_state.instrumentConfig = mock_ingredients  # Mock the nested instrumentConfig
-        mock_calibration.instrumentState = mock_instrument_state
-        mock_getCalibrationState.return_value = mock_calibration
-
         inputWorkspace = "_test_liteservice_555"
         outputWorkspace = "_test_output_lite_"
         CreateSingleValuedWorkspace(OutputWorkspace=inputWorkspace)
 
         liteDataService = LiteDataService()
         liteDataService._ensureLiteDataMap = Mock(return_value="lite_map")
+        liteDataService.dataFactoryService.getCalibrationState = Mock()
+        liteDataService.dataFactoryService.getCalibrationState.return_value.instrumentState = Mock()
+        liteDataService.dataFactoryService.getCalibrationState.return_value.instrumentState.model_dump_json.return_value = "{}"  # noqa: E501
+
         with TemporaryDirectory(dir=Resource.getPath("outputs"), suffix="/") as tmpdir:
             outputPath = Path(tmpdir, "555.nxs.h5")
             assert not outputPath.exists()
@@ -55,30 +43,21 @@ class TestLiteDataService(unittest.TestCase):
         executeRecipe.assert_called_with(
             InputWorkspace=inputWorkspace,
             OutputWorkspace=outputWorkspace,
-            LiteDataMapWorkspace=liteDataService._ensureLiteDataMap.return_value,
+            LiteDataMapWorkspace="lite_map",
             LiteInstrumentDefinitionFile=None,
-            Ingredients=mock_ingredients.model_dump_json.return_value,
+            Ingredients="{}",
         )
 
     @patch("snapred.backend.recipe.GenericRecipe.GenericRecipe.executeRecipe")
-    @patch("snapred.backend.service.LiteDataService.DataFactoryService.getCalibrationState")
-    def test_reduceLiteData_fails(self, mock_getCalibrationState, mock_executeRecipe):
+    def test_reduceLiteData_fails(self, mock_executeRecipe):
         mock_executeRecipe.return_value = {}
         mock_executeRecipe.side_effect = RuntimeError("oops!")
 
-        # Mock the calibration state and ingredients
-        mock_calibration = Mock(spec=Calibration)
-        mock_instrument_state = Mock(spec=InstrumentState)
-        mock_ingredients = Mock(spec=PixelGroupingIngredients)
-        mock_ingredients.model_dump_json.return_value = '{"instrumentConfig": {}}'  # Add appropriate JSON content
-        mock_instrument_state.instrumentConfig = mock_ingredients  # Mock the nested instrumentConfig
-        mock_calibration.instrumentState = mock_instrument_state
-        mock_getCalibrationState.return_value = mock_calibration
-
-        from snapred.backend.service.LiteDataService import LiteDataService
-
         liteDataService = LiteDataService()
-        liteDataService._ensureLiteDataMap = Mock(return_value="lite_map")
+        liteDataService._ensureLiteDataMap = Mock(return_value="lite map")
+        liteDataService.dataFactoryService.getCalibrationState = Mock()
+        liteDataService.dataFactoryService.getCalibrationState.return_value.instrumentState = Mock()
+        liteDataService.dataFactoryService.getCalibrationState.return_value.instrumentState.model_dump_json.return_value = "{}"  # noqa: E501
 
         inputWorkspace = "_test_liteservice_555"
         outputWorkspace = "_test_output_lite_"
