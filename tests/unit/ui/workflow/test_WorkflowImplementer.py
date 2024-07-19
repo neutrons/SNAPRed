266f8ba22001ad1de3eb29beadcc37d0ab9b1eef
rboston628 <bostonsr@ornl.gov>
2024-07-19 16:03:12 -0400

    add test of workflow implementer renaming on iteration

---
+++ tests/unit/ui/workflow/test_WorkflowImplementer.py
@@ -0,0 +1,54 @@
+from random import randint
+from unittest.mock import MagicMock
+
+from mantid.simpleapi import CreateSingleValuedWorkspace, GroupWorkspaces, mtd
+from qtpy.QtWidgets import QApplication
+from snapred.ui.workflow.WorkflowImplementer import WorkflowImplementer
+
+
+def test_rename_on_iterate_list():
+    """
+    Test that on iteration, a list of workspaces will be renamed according to the iteration template.
+    """
+    test_app = QApplication(["nuffink"])  # noqa
+    # setup a list of workspaces to be renamed
+    oldNames = ["old1", "old2"]
+    for name in oldNames:
+        CreateSingleValuedWorkspace(OutputWorkspace=name)
+
+    mockPresenter = MagicMock(iteration=randint(1, 120))
+
+    instance = WorkflowImplementer()
+    newNames = [instance.renameTemplate.format(workspaceName=ws, iteration=mockPresenter.iteration) for ws in oldNames]
+    instance.outputs = oldNames
+    instance._iterate(mockPresenter)
+    assert instance.collectiveOutputs == newNames
+    del test_app
+
+
+def test_rename_on_iterate_group():
+    """
+    Test that on iteration, a workspace group has all of its members renamed.
+    """
+    test_app = QApplication(["nuffink"])  # noqa
+    # setup a list of workspaces to be renamed
+    oldNames = ["parent", "child1", "child2"]
+    CreateSingleValuedWorkspace(OutputWorkspace=oldNames[-1])
+    CreateSingleValuedWorkspace(OutputWorkspace=oldNames[-2])
+    GroupWorkspaces(InputWorkspaces=oldNames[-2:], OutputWorkspace=oldNames[0])
+
+    mockPresenter = MagicMock(iteration=randint(1, 120))
+
+    instance = WorkflowImplementer()
+    newNames = [instance.renameTemplate.format(workspaceName=ws, iteration=mockPresenter.iteration) for ws in oldNames]
+    for old, new in zip(oldNames, newNames):
+        assert mtd.doesExist(old)
+        assert not mtd.doesExist(new)
+
+    instance.outputs = [oldNames[0]]
+    instance._iterate(mockPresenter)
+    assert instance.collectiveOutputs == [newNames[0]]
+    for old, new in zip(oldNames, newNames):
+        assert not mtd.doesExist(old)
+        assert mtd.doesExist(new)
+    del test_app
