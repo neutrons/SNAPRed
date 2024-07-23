56d756e6cff6af59d724c207edfa38c25332e879
Reece Boston <52183986+rboston628@users.noreply.github.com>
2024-07-23 15:06:38 -0400

    Properly iterate diagnostics :repeat_one:  (#412)
    * fix problem
    
    * add test of workflow implementer renaming on iteration
    
    * update workflow implementer test to use qtbot

--- tests/unit/backend/service/test_WorkspaceService.py
+++ tests/unit/backend/service/test_WorkspaceService.py
@@ -1,5 +1,7 @@
 from unittest.mock import MagicMock
 
+from mantid.simpleapi import CreateSingleValuedWorkspace, GroupWorkspaces, mtd
+from snapred.backend.dao.request import RenameWorkspaceFromTemplateRequest
 from snapred.backend.service.WorkspaceService import WorkspaceService
 
 
@@ -13,6 +15,53 @@ class TestWorkspaceService:
         service.groceryService = mockGroceryService
         service.rename('{"oldName": "oldName", "newName": "newName"}')
         mockGroceryService.renameWorkspace.assert_called_once_with("oldName", "newName")
+
+    def test_renameFromTemplate(self):
+        mockGroceryService = MagicMock()
+        service = WorkspaceService()
+        service.groceryService = mockGroceryService
+        # must mock out the isGroup function to return false, so that it does not look for child workspaces
+        service.groceryService.getWorkspaceForName.return_value = MagicMock(isGroup=MagicMock(return_value=False))
+        oldNames = ["old1", "old2"]
+        renameTemplate = "{workspaceName}_X"
+        newNames = [renameTemplate.format(workspaceName=ws) for ws in oldNames]
+        request = RenameWorkspaceFromTemplateRequest(
+            workspaces=oldNames,
+            renameTemplate=renameTemplate,
+        )
+        res = service.renameFromTemplate(request.model_dump_json())
+        assert newNames == res
+        mockGroceryService.renameWorkspaces.assert_called_once_with(oldNames, newNames)
+
+    def test_renameFromTemplate_group(self):
+        """
+        Create a grouped workspace with one child.
+        Make sure that a call to this endpoint will rename BOTH parent and child.
+        """
+        service = WorkspaceService()
+        oldNames = ["parent", "child"]
+        # create the needed workspaces
+        CreateSingleValuedWorkspace(OutputWorkspace="child")
+        GroupWorkspaces(
+            InputWorkspaces=["child"],
+            OutputWorkspace="parent",
+        )
+        # create the expected new names
+        renameTemplate = "{workspaceName}_X"
+        newNames = [renameTemplate.format(workspaceName=ws) for ws in oldNames]
+        for old, new in zip(oldNames, newNames):
+            assert mtd.doesExist(old)
+            assert not mtd.doesExist(new)
+        # perform the request
+        request = RenameWorkspaceFromTemplateRequest(
+            workspaces=["parent"],
+            renameTemplate=renameTemplate,
+        )
+        service.renameFromTemplate(request.model_dump_json())
+        # check names changed
+        for new in newNames:
+            assert not mtd.doesExist(old)
+            assert mtd.doesExist(new)
 
     def test_clear(self):
         mockGroceryService = MagicMock()
