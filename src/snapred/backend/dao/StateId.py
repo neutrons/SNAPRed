210ad728e60ef678ab151085b906bb1db4d8c055
Darsh <ddarsh5544@gmail.com>
2024-07-26 13:33:36 -0400

    Update to StateID

--- src/snapred/backend/dao/StateId.py
+++ src/snapred/backend/dao/StateId.py
@@ -1,12 +1,14 @@
 from dataclasses import dataclass
 
+from pydantic import Field
+
 
 # https://docs.python.org/3/library/dataclasses.html
 @dataclass
 class StateId:
     vdet_arc1: float
     vdet_arc2: float
-    WavelengthReq: float
+    WavelengthReq: float = Field(..., serialization_alias="WavelengthUserReq")
     Frequency: int
     Pos: int
 
