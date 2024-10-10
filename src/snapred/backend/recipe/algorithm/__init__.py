72d681a1bb7be6d71b23f37165f719ab0c5002e6
Reece Boston <52183986+rboston628@users.noreply.github.com>
2024-10-10 16:14:44 -0400

    Fix algo importing so that they may be imported theough `mantid.simpleapi` from other algos (#471)
    * error checking around import to handle ordering
    
    * use recursive load

--- src/snapred/backend/recipe/algorithm/__init__.py
+++ src/snapred/backend/recipe/algorithm/__init__.py
@@ -1,13 +1,36 @@
-import importlib
 import os
 from glob import glob
-
-from mantid.api import AlgorithmFactory
-from mantid.simpleapi import _create_algorithm_function
 
 moduleDir = os.path.dirname(os.path.abspath(__file__))
 modules = glob(f"{moduleDir}/*.py")
 all_module_names = [module[:-3].split("/")[-1] for module in modules if not module.endswith("__init__.py")]
+
+
+def loadModule(x):
+    import importlib
+
+    from mantid.api import AlgorithmFactory
+    from mantid.simpleapi import _create_algorithm_function
+
+    while True:
+        try:
+            module = importlib.import_module(f"{__name__}.{x}", x)
+            break
+        except ImportError as e:
+            if e.name in all_module_names:
+                loadModule(e.name)
+            continue
+
+    # get just the class name
+    x = x.split(".")[-1]
+
+    # NOTE all algorithms must have same class name as filename
+    algoClass = getattr(module, x)
+    AlgorithmFactory.subscribe(algoClass)
+    algo = algoClass()
+    algo.initialize()
+    _create_algorithm_function(x, 1, algo)
+
 
 for x in all_module_names:
     if x == "MantidSnapper":
@@ -18,21 +41,8 @@ for x in all_module_names:
         # but it also is dead code and never used
         continue
 
-    module = importlib.import_module(f"{__name__}.{x}", x)
-
-    # for the leftovers algos, this gets just the class name
-    x = x.split(".")[-1]
-
-    # NOTE all algorithms must have same class name as filename
-    algoClass = getattr(module, x)
-    AlgorithmFactory.subscribe(algoClass)
-    algo = algoClass()
-    algo.initialize()
-    _create_algorithm_function(x, 1, algo)
+    loadModule(x)
 
 # cleanup
-del _create_algorithm_function
-del AlgorithmFactory
 del glob
 del os
-del importlib
