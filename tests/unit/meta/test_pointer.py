2be25fefd045abee70c042ed264260b200e7a123
rboston628 <bostonsr@ornl.gov>
2024-10-11 15:17:11 -0400

    fixup! one more test

--- tests/unit/meta/test_pointer.py
+++ tests/unit/meta/test_pointer.py
@@ -1,10 +1,8 @@
-from unittest.mock import sentinel
-
 from snapred.meta.pointer import access_pointer, create_pointer
 
 
 def test_create_and_access_pointer():
-    a = sentinel.some_object
+    a = {"one": 2, 3: "four"}
     pa = create_pointer(a)
     aa = access_pointer(pa)
     assert a == aa
