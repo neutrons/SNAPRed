93bce353273015ade8882c5659c9fbb81e9c50e2
rboston628 <bostonsr@ornl.gov>
2024-10-14 10:28:39 -0400

    split off pixel diffcal to its own recipe

--- tests/unit/backend/recipe/test_DiffractionCalibrationRecipe.py
+++ tests/unit/backend/recipe/test_DiffractionCalibrationRecipe.py
@@ -4,12 +4,11 @@ from unittest import mock
 import pytest
 from mantid.simpleapi import mtd
 from snapred.backend.recipe.DiffractionCalibrationRecipe import DiffractionCalibrationRecipe as Recipe
-from snapred.meta.Config import Config
 from util.diffraction_calibration_synthetic_data import SyntheticData
 from util.helpers import deleteWorkspaceNoThrow
 
 ThisRecipe: str = "snapred.backend.recipe.DiffractionCalibrationRecipe"
-PixelCalAlgo: str = ThisRecipe + ".PixelDiffractionCalibration"
+PixelCalRx: str = ThisRecipe + ".PixelDiffCalRecipe"
 GroupCalAlgo: str = ThisRecipe + ".GroupDiffractionCalibration"
 
 
@@ -52,64 +51,36 @@ class TestDiffractionCalibrationRecipe(unittest.TestCase):
         assert self.recipe.groupingWS == self.fakeGroupingWorkspace
         assert self.recipe.maskWS == self.fakeMaskWorkspace
 
-    @mock.patch(PixelCalAlgo)
+    @mock.patch(PixelCalRx)
     @mock.patch(GroupCalAlgo)
-    def test_execute_successful(self, mockGroupCalAlgo, mockPixelCalAlgo):
+    def test_execute_successful(self, mockGroupCalAlgo, mockPixelRx):
         # produce 4, 2, 1, 0.5
-        mockPixelAlgo = mock.Mock()
-        mockPixelAlgo.getPropertyValue.side_effect = [f'{{"medianOffset": {4 * 2**(-i)}}}' for i in range(10)]
-        mockPixelCalAlgo.return_value = mockPixelAlgo
-        mockGroupCalAlgo.return_value.getPropertyValue.return_value = "passed"
+        mockPixelRx.return_value.cook.return_value = mock.Mock(result=True, medianOffsets=mock.sentinel.offsets)
+        mockGroupCalAlgo.return_value.getPropertyValue.return_value = mock.sentinel.group
         result = self.recipe.executeRecipe(self.fakeIngredients, self.groceryList)
         assert result["result"]
-        assert result["steps"] == [{"medianOffset": x} for x in [4, 2, 1, 0.5]]
-        assert result["calibrationTable"] == "passed"
-        assert result["outputWorkspace"] == "passed"
-        assert result["maskWorkspace"] == "passed"
+        assert result["steps"] == mock.sentinel.offsets
+        assert result["calibrationTable"] == mock.sentinel.group
+        assert result["outputWorkspace"] == mock.sentinel.group
+        assert result["maskWorkspace"] == mock.sentinel.group
 
-    @mock.patch(PixelCalAlgo)
-    def test_execute_unsuccessful_pixel_cal(self, mockPixelCalAlgo):
-        mockPixelAlgo = mock.Mock()
-        mockPixelAlgo.execute.side_effect = RuntimeError("failure in pixel algo")
-        mockPixelCalAlgo.return_value = mockPixelAlgo
+    @mock.patch(PixelCalRx)
+    def test_execute_unsuccessful_pixel_cal(self, mockPixelRx):
+        mockPixelRx.return_value.cook.return_value = mock.Mock(result=False)
         with pytest.raises(RuntimeError) as e:
             self.recipe.executeRecipe(self.fakeIngredients, self.groceryList)
-        assert str(e.value) == "failure in pixel algo"
+        assert str(e.value) == "Pixel Calibration failed"
 
-    @mock.patch(PixelCalAlgo)
+    @mock.patch(PixelCalRx)
     @mock.patch(GroupCalAlgo)
-    def test_execute_unsuccessful_group_cal(self, mockGroupCalAlgo, mockPixelCalAlgo):
-        mockPixelAlgo = mock.Mock()
-        mockPixelAlgo.getPropertyValue.return_value = '{"medianOffset": 0}'
-        mockPixelCalAlgo.return_value = mockPixelAlgo
+    def test_execute_unsuccessful_group_cal(self, mockGroupCalAlgo, mockPixelRx):
+        mockPixelRx.return_value.cook.return_value = mock.Mock(result=True, medianOffsets=[0])
         mockGroupAlgo = mock.Mock()
-        mockGroupAlgo.execute.side_effect = RuntimeError("failure in group algo")
+        mockGroupAlgo.execute.side_effect = RuntimeError(mock.sentinel.group)
         mockGroupCalAlgo.return_value = mockGroupAlgo
         with pytest.raises(RuntimeError) as e:
             self.recipe.executeRecipe(self.fakeIngredients, self.groceryList)
-        assert str(e.value) == "failure in group algo"
-
-    @mock.patch(PixelCalAlgo)
-    @mock.patch(GroupCalAlgo)
-    def test_execute_unsuccessful_later_calls(self, mockGroupCalAlgo, mockPixelCalAlgo):
-        # this will check that errors are raised in each of the three try-catch blocks
-        # the mocked median offset will follow pattern 1, 0.5, 0.25, etc.
-        # first time, algo fails (first try block)
-        # second time, algo succeeds with 1, then fails (second try block)
-        # second time, algo succeeds with 1, succeeds with 0.5, then fails (third try block)
-        mockAlgo = mock.Mock()
-        mockGroupCalAlgo.return_value = mockAlgo
-        mockPixelCalAlgo.return_value = mockAlgo
-        for i in range(3):
-            # algo succeeds once then raises an error
-            listOfFails = [1] * (i)
-            listOfFails.append(RuntimeError(f"passed {i}"))
-            mockAlgo.execute.side_effect = listOfFails
-            # algo will return 1, 0.5, 0.25, etc.
-            mockAlgo.getPropertyValue.side_effect = [f'{{"medianOffset": {2**(-i)}}}' for i in range(10)]
-            with pytest.raises(RuntimeError) as e:
-                self.recipe.executeRecipe(self.fakeIngredients, self.groceryList)
-            assert str(e.value).split("\n")[0] == f"passed {i}"
+        assert e.value.args == (mock.sentinel.group,)
 
     def test_execute_with_algos(self):
         # create sample data
@@ -131,43 +102,4 @@ class TestDiffractionCalibrationRecipe(unittest.TestCase):
         assert res["maskWorkspace"]
         mask = mtd[res["maskWorkspace"]]
         assert mask.getNumberMasked() == 0
-        assert res["steps"][-1]["medianOffset"] <= self.fakeIngredients.convergenceThreshold
-
-    @mock.patch(PixelCalAlgo)
-    @mock.patch(GroupCalAlgo)
-    def test_hard_cap_at_five(self, mockGroupAlgo, mockPixelAlgo):
-        mockAlgo = mock.Mock()
-        mockAlgo.getPropertyValue.side_effect = [f'{{"medianOffset": {11-i}}}' for i in range(10)]
-        mockPixelAlgo.return_value = mockAlgo
-        mockGroupAlgo.return_value.getPropertyValue.return_value = "fake"
-        result = self.recipe.executeRecipe(self.fakeIngredients, self.groceryList)
-        assert result["result"]
-        maxIterations = Config["calibration.diffraction.maximumIterations"]
-        assert result["steps"] == [{"medianOffset": 11 - i} for i in range(maxIterations)]
-        assert result["calibrationTable"] == "fake"
-        assert result["outputWorkspace"] == "fake"
-        assert result["maskWorkspace"] == "fake"
-        # change the config then run again
-        maxIterations = 7
-        mockAlgo.getPropertyValue.side_effect = [f'{{"medianOffset": {11-i}}}' for i in range(10)]
-        Config._config["calibration"]["diffraction"]["maximumIterations"] = maxIterations
-        result = self.recipe.executeRecipe(self.fakeIngredients, self.groceryList)
-        assert result["result"]
-        assert result["steps"] == [{"medianOffset": 11 - i} for i in range(maxIterations)]
-        assert result["calibrationTable"] == "fake"
-        assert result["outputWorkspace"] == "fake"
-        assert result["maskWorkspace"] == "fake"
-
-    @mock.patch(PixelCalAlgo)
-    @mock.patch(GroupCalAlgo)
-    def test_ensure_monotonic(self, mockGroupAlgo, mockPixelAlgo):
-        mockAlgo = mock.Mock()
-        mockAlgo.getPropertyValue.side_effect = [f'{{"medianOffset": {i}}}' for i in [2, 1, 2, 0]]
-        mockPixelAlgo.return_value = mockAlgo
-        mockGroupAlgo.return_value.getPropertyValue.return_value = "fake"
-        result = self.recipe.executeRecipe(self.fakeIngredients, self.groceryList)
-        assert result["result"]
-        assert result["steps"] == [{"medianOffset": i} for i in [2, 1]]
-        assert result["calibrationTable"] == "fake"
-        assert result["outputWorkspace"] == "fake"
-        assert result["maskWorkspace"] == "fake"
+        assert res["steps"][-1] <= self.fakeIngredients.convergenceThreshold
