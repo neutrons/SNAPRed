53e9326843b2c4beda7d8dd46bda7fe811aa9979
Michael Walsh <68125095+walshmm@users.noreply.github.com>
2025-02-18 13:09:33 -0500

    mutex savenexus calls to prevent segfaults (#544)
    * mutex savenexus calls to prevent segfaults
    
    add back the sleeps
    
    remove algo from manager when complete
    
    added timeout in case of long async algo queue
    
    reorg where we check and wait for algos to complete, share a lock between potential cross interference
    
    both reentrant and nonconcurrent mutexes
    
    * cleanup timeout member, add some tests
    
    * only apply wait and remove to nonconcurrent case
    
    * update tests to account for timeout only applying to nonconcurrent algos
    
    * fix spelling mistake

--- tests/unit/backend/recipe/algorithm/test_MantidSnapper.py
+++ tests/unit/backend/recipe/algorithm/test_MantidSnapper.py
@@ -1,10 +1,13 @@
 import unittest
 from unittest import mock
 
+import pytest
 from mantid.kernel import Direction
 
 from snapred.backend.recipe.algorithm.MantidSnapper import MantidSnapper
 from snapred.meta.Callback import callback
+
+PatchRoot: str = "snapred.backend.recipe.algorithm.MantidSnapper.{0}"
 
 
 class TestMantidSnapper(unittest.TestCase):
@@ -17,7 +20,7 @@ class TestMantidSnapper(unittest.TestCase):
         self.fakeFunction.getProperties.return_value = [self.fakeOutput]
         self.fakeFunction.getProperty.return_value = self.fakeOutput
 
-    @mock.patch("snapred.backend.recipe.algorithm.MantidSnapper.AlgorithmManager")
+    @mock.patch(PatchRoot.format("AlgorithmManager"))
     def test_snapper_fake_algo(self, mock_AlgorithmManager):
         mock_AlgorithmManager.create.return_value = self.fakeFunction
         return_of_algo = "return of algo"
@@ -25,7 +28,7 @@ class TestMantidSnapper(unittest.TestCase):
         test = mantidSnapper.fakeFunction("test", fakeOutput=return_of_algo)
         assert str(test.__class__) == str(callback(return_of_algo.__class__).__class__)
 
-    @mock.patch("snapred.backend.recipe.algorithm.MantidSnapper.AlgorithmManager")
+    @mock.patch(PatchRoot.format("AlgorithmManager"))
     def test_snapper_fake_queue(self, mock_AlgorithmManager):
         mock_AlgorithmManager.create.return_value = self.fakeFunction
         return_of_algo = "return of algo"
@@ -33,3 +36,53 @@ class TestMantidSnapper(unittest.TestCase):
         mantidSnapper.fakeFunction("test", fakeOutput=return_of_algo)
         mantidSnapper.executeQueue()
         assert self.fakeFunction.execute.called
+
+    @mock.patch(PatchRoot.format("AlgorithmManager"))
+    def test_timeout(self, mockAlgorithmManager):
+        mockAlgorithmManager.runningInstancesOf = mock.Mock(return_value=["theAlgoThatNeverEnds"])
+        mockAlgorithmManager.create.return_value = self.fakeFunction
+
+        mantidSnapper = MantidSnapper(parentAlgorithm=None, name="")
+        mantidSnapper.timeout = 0.2
+        mantidSnapper.fakeFunction("test", fakeOutput="output")
+        mantidSnapper._nonConcurrentAlgorithms = mantidSnapper._nonConcurrentAlgorithms + ("fakeFunction",)
+        mantidSnapper._nonConcurrentAlgorithmMutex = mock.Mock()
+
+        with pytest.raises(TimeoutError, match="Timeout occurred while waiting for instance of"):
+            mantidSnapper.executeQueue()
+
+    @mock.patch(PatchRoot.format("AlgorithmManager"))
+    def test_timeout_concurrent(self, mockAlgorithmManager):
+        mockAlgorithmManager.runningInstancesOf = mock.Mock(return_value=["theAlgoThatNeverEnds"])
+        mockAlgorithmManager.create.return_value = self.fakeFunction
+
+        mantidSnapper = MantidSnapper(parentAlgorithm=None, name="")
+        mantidSnapper.timeout = 0.2
+        mantidSnapper.fakeFunction("test", fakeOutput="output")
+        mantidSnapper._nonConcurrentAlgorithmMutex = mock.Mock()
+
+        mantidSnapper.executeQueue()
+        assert not mantidSnapper._nonConcurrentAlgorithmMutex.acquire.called
+
+    @mock.patch(PatchRoot.format("AlgorithmManager"))
+    def test_mutexIsObtained_nonConcurrent(self, mockAlgorithmManager):
+        mockAlgorithmManager.create.return_value = self.fakeFunction
+        mantidSnapper = MantidSnapper(parentAlgorithm=None, name="")
+        mantidSnapper.fakeFunction("test", fakeOutput="output")
+        mantidSnapper._nonConcurrentAlgorithms = mantidSnapper._nonConcurrentAlgorithms + ("fakeFunction",)
+        mantidSnapper._nonConcurrentAlgorithmMutex = mock.Mock()
+
+        mantidSnapper.executeQueue()
+        assert mantidSnapper._nonConcurrentAlgorithmMutex.acquire.called
+        assert mantidSnapper._nonConcurrentAlgorithmMutex.release.called
+
+    @mock.patch(PatchRoot.format("AlgorithmManager"))
+    def test_mutexIsObtained_nonReentrant(self, mockAlgorithmManager):
+        mockAlgorithmManager.create.return_value = self.fakeFunction
+        mantidSnapper = MantidSnapper(parentAlgorithm=None, name="")
+        mantidSnapper.fakeFunction("test", fakeOutput="output")
+        mantidSnapper._nonReentrantMutexes["fakeFunction"] = mock.Mock()
+
+        mantidSnapper.executeQueue()
+        assert mantidSnapper._nonReentrantMutexes["fakeFunction"].acquire.called
+        assert mantidSnapper._nonReentrantMutexes["fakeFunction"].release.called
