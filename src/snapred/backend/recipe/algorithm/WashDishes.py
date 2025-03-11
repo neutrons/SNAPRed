a612493303e13f04276e8660a3dc76c5dba83f69
Darsh <ddarsh5544@gmail.com>
2025-03-11 16:13:20 -0400

    Correct logic for testing

--- src/snapred/backend/recipe/algorithm/WashDishes.py
+++ src/snapred/backend/recipe/algorithm/WashDishes.py
@@ -18,24 +18,23 @@ class WashDishes(PythonAlgorithm):
         # declare properties
         self.declareProperty("Workspace", defaultValue="", direction=Direction.Input)  # noqa: F821
         self.declareProperty(StringArrayProperty(name="WorkspaceList", values=[], direction=Direction.Input))
+        self.setRethrows(True)
 
         self.cis_enabled: bool = Config["cis_mode.enabled"]
         self.cis_preserve: bool = Config["cis_mode.preserveDiagnosticWorkspaces"]
-
-        self.setRethrows(True)
 
     def PyExec(self) -> None:
         self.log().notice("Washing the dishes...")
 
         workspace = self.getProperty("Workspace").value
         if workspace and mtd.doesExist(workspace):
-            if not self.cis_enabled and not self.cis_preserve:
+            if (not self.cis_enabled) or (not self.cis_preserve):
                 DeleteWorkspace(Workspace=workspace)
 
         workspaces = self.getProperty("WorkspaceList").value
         workspaces = [w for w in workspaces if mtd.doesExist(w)]
         if workspaces:
-            if not self.cis_enabled and not self.cis_preserve:
+            if (not self.cis_enabled) or (not self.cis_preserve):
                 DeleteWorkspaces(WorkspaceList=workspaces)
 
 
