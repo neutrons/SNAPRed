d6993960701b29246f2ae829280dd9535841b495
Michael Walsh <walshmm@ornl.gov>
2025-04-03 11:20:36 -0400

    remove redundant test

--- tests/unit/backend/recipe/test_RebinFocussedGroupDataRecipe.py
+++ tests/unit/backend/recipe/test_RebinFocussedGroupDataRecipe.py
@@ -1,6 +1,5 @@
 import unittest
 
-import pytest
 from mantid.api import MatrixWorkspace
 from mantid.simpleapi import (
     CreateSampleWorkspace,
@@ -8,11 +7,9 @@ from mantid.simpleapi import (
     GroupDetectors,
     mtd,
 )
-from util.Config_helpers import Config_override
 from util.dao import DAOFactory
 from util.SculleryBoy import SculleryBoy
 
-from snapred.backend.dao.Limit import Limit
 from snapred.backend.recipe.RebinFocussedGroupDataRecipe import RebinFocussedGroupDataRecipe as Recipe
 
 ThisRecipe: str = "snapred.backend.recipe.RebinFocussedGroupDataRecipe"
@@ -51,21 +48,3 @@ class TestRebinFocussedGroupDataRecipe(unittest.TestCase):
         outputWs = mtd[self.sampleWorkspace]
         assert isinstance(outputWs, MatrixWorkspace)
         assert outputWs.isRaggedWorkspace()
-
-    def test_bad_dspacing_range(self):
-        pixel_group = self.sculleryBoy.prepPixelGroup()
-
-        first_group_id = list(pixel_group.pixelGroupingParameters.keys())[0]
-        group_params = pixel_group.pixelGroupingParameters[first_group_id]
-
-        group_params.dResolution = Limit[float](minimum=0.1, maximum=2.0)
-
-        ingredients = Recipe.Ingredients(pixelGroup=pixel_group, preserveEvents=True)
-        recipe = Recipe()
-
-        with (
-            Config_override("constants.CropFactors.lowdSpacingCrop", 3.0),
-            Config_override("constants.CropFactors.highdSpacingCrop", 0.0),
-            pytest.raises(ValueError, match="Invalid d-spacing range detected: some dMax <= dMin."),
-        ):
-            recipe.chopIngredients(ingredients)
