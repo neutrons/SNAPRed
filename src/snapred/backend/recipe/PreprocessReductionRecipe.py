f4ff5a0fbe1d136effd7724edf90eba57171c243
mguthriem <guthriem@ornl.gov>
2025-04-25 16:51:40 -0400

    prototype that applies any swiss cheese mask found in the ADS

--- src/snapred/backend/recipe/PreprocessReductionRecipe.py
+++ src/snapred/backend/recipe/PreprocessReductionRecipe.py
@@ -1,4 +1,5 @@
 from typing import Any, Dict, List, Set, Tuple
+from mantid.simpleapi import mtd
 
 from snapred.backend.dao.ingredients import PreprocessReductionIngredients as Ingredients
 from snapred.backend.log.logger import snapredLogger
@@ -32,6 +33,21 @@ class PreprocessReductionRecipe(Recipe[Ingredients]):
         self.diffcalWs = groceries.get("diffcalWorkspace", "")
         self.outputWs = groceries.get("outputWorkspace", groceries["inputWorkspace"])
 
+    def findMaskBinsTableWorkspaces(self):
+        """
+        Locates bin Mask workspaces on the basis of their names and creates a list of these
+        """
+
+        self.binMasks = []
+        for ws in mtd.getObjectNames():
+            if "maskBins_" in ws:
+                self.binMasks.append(ws)
+        print(f"{len(self.binMasks)} binMasks were found in ADS")
+        if len(self.binMasks) >= 1:
+            self.hasBinMasks = True
+        else:
+            self.hasBinMasks = False
+    
     def queueAlgos(self):
         """
         Queues up the processing algorithms for the recipe.
@@ -51,6 +67,36 @@ class PreprocessReductionRecipe(Recipe[Ingredients]):
                 InstrumentWorkspace=self.outputWs,
                 CalibrationWorkspace=self.diffcalWs,
             )
+
+        #check if any bin masks exist
+        self.findMaskBinsTableWorkspaces()
+        #apply bin Masks if they were found
+        if self.hasBinMasks:
+            for mask in self.binMasks:
+                #extract units from ws name (table workspaces don't have logs)
+                maskUnits = mask.split('_')[-1]
+                #ensure units of workspace match
+                self.mantidSnapper.ConvertUnits(
+                    f"Converting units to match Bin Mask with units of {maskUnits}",
+                    InputWorkspace=self.outputWs,
+                    Target = maskUnits,
+                    OutputWorkspace=self.outputWs
+                )
+                #mask bins
+                self.mantidSnapper.MaskBinsFromTable(
+                    "Masking bins...",
+                    InputWorkspace=self.outputWs,
+                    MaskingInformation=mask,
+                    OutputWorkspace=self.outputWs
+                )
+
+        # convert to tof if needed
+        self.mantidSnapper.ConvertUnits(
+            "Converting to TOF...",
+            InputWorkspace=self.outputWs,
+            Target="TOF",
+            OutputWorkspace=self.outputWs,
+        )
 
     def cook(self, ingredients: Ingredients, groceries: Dict[str, str]) -> Dict[str, Any]:
         """
