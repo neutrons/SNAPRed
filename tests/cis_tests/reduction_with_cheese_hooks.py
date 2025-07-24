d667b1539327d8a33bd45d807987d40c11b8afb0
Michael Walsh <walshmm@ornl.gov>
2025-07-24 13:40:50 -0400

    update the cheese hooks example to encourage staticmethods

--- tests/cis_tests/reduction_with_cheese_hooks.py
+++ tests/cis_tests/reduction_with_cheese_hooks.py
@@ -19,29 +19,33 @@ groups = reductionService.loadAllGroupings(runId, useLiteMode)
 # find column group
 columnGroup = next((group for group in groups["focusGroups"] if "column" in group.name.lower()), None)
 
-# NOTE: Be careful how you define hook methods.  They should not be class methods.
-def cheeseHook(self, cheeseMasks: list[str]):
-    # raise RuntimeError(f"Hook Executed!: {cheeseMasks}")
-    for mask in cheeseMasks:
-        #extract units from ws name (table workspaces don't have logs)
-        maskUnits = mask.split('_')[-1]
-        #ensure units of workspace match
-        self.mantidSnapper.ConvertUnits(
-            f"Converting units to match Bin Mask with units of {maskUnits}",
-            InputWorkspace=self.outputWs,
-            Target = maskUnits,
-            OutputWorkspace=self.outputWs
-        )
-        #mask bins
-        self.mantidSnapper.MaskBinsFromTable(
-            "Masking bins...",
-            InputWorkspace=self.outputWs,
-            MaskingInformation=mask,
-            OutputWorkspace=self.outputWs
-        )
-    self.mantidSnapper.executeQueue()
+# NOTE: Be careful how you define hook methods.  
+#       They should not be class/bound methods. (i.e. no self or cls parameters)
+#       @staticmethods are encouraged, module level functions are fine.
+class HookCollection:
+    @staticmethod
+    def cheeseHook(context, cheeseMasks: list[str]):
+        # raise RuntimeError(f"Hook Executed!: {cheeseMasks}")
+        for mask in cheeseMasks:
+            #extract units from ws name (table workspaces don't have logs)
+            maskUnits = mask.split('_')[-1]
+            #ensure units of workspace match
+            context.mantidSnapper.ConvertUnits(
+                f"Converting units to match Bin Mask with units of {maskUnits}",
+                InputWorkspace=context.outputWs,
+                Target = maskUnits,
+                OutputWorkspace=context.outputWs
+            )
+            #mask bins
+            context.mantidSnapper.MaskBinsFromTable(
+                "Masking bins...",
+                InputWorkspace=context.outputWs,
+                MaskingInformation=mask,
+                OutputWorkspace=context.outputWs
+            )
+        context.mantidSnapper.executeQueue()
     
-hook = Hook(func=cheeseHook,cheeseMasks=[])
+hook = Hook(func=HookCollection.cheeseHook,cheeseMasks=[])
 
 hooks = {
     "PostPreprocessReductionRecipe" : [hook, hook],
