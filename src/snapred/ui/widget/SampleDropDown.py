6cfc34dbaffdf889f8b9eb74710e1ad4a0fe3bfa
Michael Walsh <walshmm@ornl.gov>
2025-09-26 10:34:23 -0400

    handle sample dropdown enabling through signal

--- src/snapred/ui/widget/SampleDropDown.py
+++ src/snapred/ui/widget/SampleDropDown.py
@@ -1,9 +1,14 @@
+
+from qtpy.QtCore import Signal, Slot
 from qtpy.QtWidgets import QComboBox, QVBoxLayout
 
 from snapred.ui.widget.SNAPWidget import SNAPWidget
 
 
 class SampleDropDown(SNAPWidget):
+    
+    signalSetEnabled = Signal(bool)
+    
     def __init__(self, label, items=[], parent=None):
         super(SampleDropDown, self).__init__(parent)
         self._label = label
@@ -16,8 +21,14 @@ class SampleDropDown(SNAPWidget):
 
         self._layout = QVBoxLayout(self)
         self._layout.addWidget(self.dropDown)
+        
+        self.signalSetEnabled.connect(self._setEnabled)
 
     def setEnabled(self, flag: bool):
+        self.signalSetEnabled.emit(flag)
+
+    @Slot(bool)
+    def _setEnabled(self, flag: bool):
         if flag:
             self.dropDown.setVisible(True)
             self._disabledField.setVisible(False)
